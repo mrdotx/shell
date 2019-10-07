cce6e1fdf4c0eb2a3fed7e1c7635757c1eae5fa8
mrdotx <klassiker@gmx.de>
2019-10-07 08:23:22 +0200

    git reorganization

---
+++ historymaintenance.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+# vim:fileencoding=utf-8:ft=sh:foldmethod=marker
+
+# Path:     ~/coding/shell/historymaintenance.sh
+# User:     klassiker [mrdotx]
+# GitHub:   https://github.com/mrdotx/shell
+
+# color variables {{{
+#black=$(tput setaf 0)
+#red=$(tput setaf 1)
+#green=$(tput setaf 2)
+#yellow=$(tput setaf 3)
+#blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+#cyan=$(tput setaf 6)
+#white=$(tput setaf 7)
+reset=$(tput sgr0)
+# }}}
+
+# procedure {{{
+echo "[${magenta}bash${reset}] remove white space from end of line"
+sed -i "s/ *$//" "$HOME"/.bash_history
+
+echo "[${magenta}bash${reset}] sort and remove duplicates"
+sort -u "$HOME"/.bash_history | sort -o "$HOME"/.bash_history
+
+echo "[${magenta}zsh${reset}] remove white space from end of line"
+sed -i "s/ *$//" "$HOME"/.zsh_history
+
+echo "[${magenta}zsh${reset}] sort and remove duplicates"
+sort -t ";" -k 2 -u "$HOME"/.zsh_history | sort -o "$HOME"/.zsh_history
+# }}}
