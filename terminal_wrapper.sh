b8201abe6293e9be7bdb13d9a4364c9b4f173109
mrdotx <klassiker@gmx.de>
2019-10-07 10:13:29 +0200

    git reorganization

---
+++ terminal_wrapper.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# vim:fileencoding=utf-8:ft=sh:foldmethod=marker
+
+# Path:     ~/coding/shell/terminal_wrapper.sh
+# User:     klassiker [mrdotx]
+# GitHub:   https://github.com/mrdotx/shell
+
+# procedure {{{
+if [ $# -eq 0 ]; then "${SHELL:-bash}"; else "$@"; fi
+echo "The command exited with status $?. Press Enter to close $TERMINAL."
+read line
+# }}}
