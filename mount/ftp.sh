b8cd8556ea7b962d0d8c05d2847a588e9beb7838
mrdotx <klassiker@gmx.de>
2019-11-09 22:48:55 +0100

    update structure

--- mount/ftp.sh
+++ mount/ftp.sh
@@ -4,12 +4,12 @@
 # path:       ~/coding/shell/mount/ftp.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-09 22:06:31
+# date:       2019-11-09 22:48:32
 
-# connect to ftp via curlftpfs
 ftpdir=$1
 ftphost=<Hostname>
 ftpuser=<Username>
 ftppass=$(gpg -q --decrypt <passwordfile.gpg>)
 
+# connect to ftp via curlftpfs
 curlftpfs $ftphost "$ftpdir" -o user=$ftpuser:$ftppass && exit 0
--- mount/sftp.sh
+++ mount/sftp.sh
@@ -4,13 +4,13 @@
 # path:       ~/coding/shell/mount/sftp.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-09 22:08:37
+# date:       2019-11-09 22:48:48
 
-# connect to sftp via sshfs
-# host must be in .ssh/known_hosts
 sftpdir=$1
 sftphost=<Hostname>
 sftpuser=<Username>
 sftppass=$(gpg -q --decrypt <passwordfile.gpg>)
 
+# connect to sftp via sshfs
+# host must be in .ssh/known_hosts
 sshfs -o password_stdin $sftpuser@$sftphost:/ "$sftpdir" <<< "$sftppass" && exit 0
