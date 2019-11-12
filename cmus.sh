e9881d6dc2a5131a194250f6b961cde27f7feb3a
mrdotx <klassiker@gmx.de>
2019-11-12 16:45:18 +0100

    change to toggle to scratchpad

--- cmus.sh
+++ cmus.sh
@@ -4,11 +4,11 @@
 # path:       ~/coding/shell/cmus.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-12 12:50:55
+# date:       2019-11-12 16:18:08
 
-# start cmus, if its running toggle pause/play 
+# start cmus, if its running toggle from/to i3 scratchpad
 if ! pgrep -x cmus ; then
     cmus
 else
-    cmus-remote -u
+    i3-msg [title=cmus] scratchpad show
 fi
