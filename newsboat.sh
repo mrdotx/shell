bd7e5dacaa1cb81487f7b0d5cdbb5299f41317ad
mrdotx <klassiker@gmx.de>
2019-11-15 20:01:09 +0100

    change output

--- newsboat.sh
+++ newsboat.sh
@@ -4,7 +4,7 @@
 # path:       ~/coding/shell/newsboat.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-15 19:34:56
+# date:       2019-11-15 19:46:15
 
 # reload feeds and compact the cache
-ping -c1 -W1 -q google.com &> /dev/null && newsboat -x reload && newsboat -q -X && notify-send "Newsboat" "Updated and adjusted!" --icon=messagebox_info || notify-send "Newsboat" "Problems update the feeds!" --icon=messagebox_warning
+ping -c1 -W1 -q google.com &> /dev/null && newsboat -x reload && newsboat -q -X &> /dev/null && notify-send "Newsboat" "Updated and adjusted!" --icon=messagebox_info || notify-send "Newsboat" "Problems update the feeds!" --icon=messagebox_warning
