6e43b4db5271c84664f3e46e2096d542f1ac6196
mrdotx <klassiker@gmx.de>
2019-11-15 19:35:11 +0100

    create

---
+++ newsboat.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# vim:fileencoding=utf-8:ft=sh:foldmethod=marker
+
+# path:       ~/coding/shell/newsboat.sh
+# user:       klassiker [mrdotx]
+# github:     https://github.com/mrdotx/shell
+# date:       2019-11-15 19:34:56
+
+# reload feeds and compact the cache
+ping -c1 -W1 -q google.com &> /dev/null && newsboat -x reload && newsboat -q -X && notify-send "Newsboat" "Updated and adjusted!" --icon=messagebox_info || notify-send "Newsboat" "Problems update the feeds!" --icon=messagebox_warning
