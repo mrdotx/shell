a65c25b73c2b4482326298080e446aa93b9563d2
mrdotx <klassiker@gmx.de>
2019-11-17 16:04:16 +0100

    add caution icon

--- newsboat.sh
+++ newsboat.sh
@@ -4,7 +4,7 @@
 # path:       ~/coding/shell/newsboat.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-17 11:51:14
+# date:       2019-11-17 16:03:54
 
 # reload feeds and compact the cache
-ping -c1 -W1 -q google.com &> /dev/null && newsboat -x reload && newsboat -q -X &> /dev/null && notify-send -i "$HOME/coding/shell/icons/rss.png" "Newsboat" "Updated and adjusted!" || notify-send "Newsboat" "Problems update the feeds!" --icon=messagebox_warning
+ping -c1 -W1 -q google.com &> /dev/null && newsboat -x reload && newsboat -q -X &> /dev/null && notify-send -i "$HOME/coding/shell/icons/rss.png" "Newsboat" "Updated and adjusted!" || notify-send -i "$HOME/coding/shell/icons/caution.png" "Newsboat" "Problem update the feeds!"
