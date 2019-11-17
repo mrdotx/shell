7db11efc8c33569f98cab3bcd54dae0c57500655
mrdotx <klassiker@gmx.de>
2019-11-17 16:57:11 +0100

    change vm directory

--- backup.sh
+++ backup.sh
@@ -4,11 +4,11 @@
 # path:       ~/coding/shell/backup.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-09 21:52:39
+# date:       2019-11-17 16:56:47
 
 backupname="morpheus"
 localhome="/home/klassiker"
 remotelocation="alarm@prometheus:/home/alarm/backup/$backupname/"
 
 # backup / to remote location (for testing rsync option --dry-run)
-sudo rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","localhome/mount/*","$localhome/vm/*","/mnt/*","/media/*","/lost+found"} / $remotelocation
+sudo rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","localhome/mount/*","$localhome/VM/*","/mnt/*","/media/*","/lost+found"} / $remotelocation
