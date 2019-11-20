e806bf39b9f15052cef7797dedd54de21ac605ff
mrdotx <klassiker@gmx.de>
2019-11-20 19:57:21 +0100

    change podcasts

--- linkhandler.sh
+++ linkhandler.sh
@@ -4,10 +4,11 @@
 # path:       ~/coding/shell/linkhandler.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-12 22:13:51
+# date:       2019-11-20 19:18:37
 
-pictures="sxiv -a -s f"
 movies="mpv --quiet"
+podcasts="$TERMINAL -e mpv --no-audio-display"
+pictures="sxiv -a -s f"
 
 # if no url given open browser
 [ -z "$1" ] && {
@@ -19,11 +20,11 @@ case "$1" in
 *mkv | *webm | *mp4 | *youtube.com/watch* | *youtube.com/playlist* | *youtu.be*)
     setsid tsp $movies --input-ipc-server=/tmp/mpvsoc$(date +%s) "$1" >/dev/null 2>&1 &
     ;;
+*mp3 | *flac | *opus)
+    setsid tsp $podcasts --input-ipc-server=/tmp/mpvsoc$(date +%s) "$1" >/dev/null 2>&1 &
+    ;;
 *png | *jpg | *jpe | *jpeg | *gif)
     curl -sL "$1" > "/tmp/$(echo "$1" | sed "s/.*\///")" && $pictures "/tmp/$(echo "$1" | sed "s/.*\///")"  >/dev/null 2>&1 &
-    ;;
-*mp3 | *flac | *opus | *mp3?source)
-    setsid tsp curl -LO "$1" >/dev/null 2>&1 &
     ;;
 *)
     if [ -f "$1" ]; then "$TERMINAL" -e "$EDITOR $1"
