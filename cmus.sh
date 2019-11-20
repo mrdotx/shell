200511979e888907bfe1b680d3493db38e8ea338
mrdotx <klassiker@gmx.de>
2019-11-20 13:43:28 +0100

    add instance

--- cmus.sh
+++ cmus.sh
@@ -4,11 +4,11 @@
 # path:       ~/coding/shell/cmus.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-12 16:18:08
+# date:       2019-11-20 13:38:46
 
 # start cmus, if its running toggle from/to i3 scratchpad
 if ! pgrep -x cmus ; then
     cmus
 else
-    i3-msg [title=cmus] scratchpad show
+    i3-msg [instance=$TERMINAL title=cmus] scratchpad show
 fi
