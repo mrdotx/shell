5b290b34ef19c9e95010836ffe7bcf0bc6a9e97d
mrdotx <klassiker@gmx.de>
2019-11-21 13:35:26 +0100

    add notification

--- linkhandler.sh
+++ linkhandler.sh
@@ -4,7 +4,7 @@
 # path:       ~/coding/shell/linkhandler.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-20 19:18:37
+# date:       2019-11-21 13:34:18
 
 movies="mpv --quiet"
 podcasts="$TERMINAL -e mpv --no-audio-display"
@@ -12,22 +12,31 @@ pictures="sxiv -a -s f"
 
 # if no url given open browser
 [ -z "$1" ] && {
-    "$BROWSER"
+    notify-send -i "$HOME/coding/shell/icons/rss.png" "Newsboat" "No URL given, open browser!" &&
+        "$BROWSER"
     exit
 }
 
 case "$1" in
 *mkv | *webm | *mp4 | *youtube.com/watch* | *youtube.com/playlist* | *youtu.be*)
-    setsid tsp $movies --input-ipc-server=/tmp/mpvsoc$(date +%s) "$1" >/dev/null 2>&1 &
+    notify-send -i "$HOME/coding/shell/icons/rss.png" "Newsboat" "Open in multimedia player!" &&
+        setsid tsp $movies --input-ipc-server=/tmp/mpvsoc$(date +%s) "$1" >/dev/null 2>&1 &
     ;;
 *mp3 | *flac | *opus)
-    setsid tsp $podcasts --input-ipc-server=/tmp/mpvsoc$(date +%s) "$1" >/dev/null 2>&1 &
+    notify-send -i "$HOME/coding/shell/icons/rss.png" "Newsboat" "Open in multimedia player!" &&
+        setsid tsp $podcasts --input-ipc-server=/tmp/mpvsoc$(date +%s) "$1" >/dev/null 2>&1 &
     ;;
 *png | *jpg | *jpe | *jpeg | *gif)
-    curl -sL "$1" > "/tmp/$(echo "$1" | sed "s/.*\///")" && $pictures "/tmp/$(echo "$1" | sed "s/.*\///")"  >/dev/null 2>&1 &
+    notify-send -i "$HOME/coding/shell/icons/rss.png" "Newsboat" "Open in picture viewer!" &&
+        curl -sL "$1" >"/tmp/$(echo "$1" | sed "s/.*\///")" && $pictures "/tmp/$(echo "$1" | sed "s/.*\///")" >/dev/null 2>&1 &
     ;;
 *)
-    if [ -f "$1" ]; then "$TERMINAL" -e "$EDITOR $1"
-    else setsid $BROWSER "$1" >/dev/null 2>&1 & fi
+    if [ -f "$1" ]; then
+        notify-send -i "$HOME/coding/shell/icons/rss.png" "Newsboat" "Open in editor!" &&
+            setsid "$TERMINAL" -e "$EDITOR" "$1" &
+    else
+        notify-send -i "$HOME/coding/shell/icons/rss.png" "Newsboat" "Open in browser!" &&
+            setsid "$BROWSER" "$1" >/dev/null 2>&1 &
+    fi
     ;;
 esac
