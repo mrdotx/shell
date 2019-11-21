2e1574765e65671edda3588ae232dd588d9405fa
mrdotx <klassiker@gmx.de>
2019-11-21 01:45:31 +0100

    change swapfile

--- backup.sh
+++ backup.sh
@@ -4,11 +4,11 @@
 # path:       ~/coding/shell/backup.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-17 16:59:28
+# date:       2019-11-21 01:34:15
 
 backupname="morpheus"
 localhome="/home/klassiker"
 remotelocation="alarm@prometheus:/home/alarm/backup/$backupname/"
 
 # backup / to remote location (for testing rsync option --dry-run)
-sudo rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","$localhome/mount/*","$localhome/VM/*","/mnt/*","/media/*","/lost+found"} / $remotelocation
+sudo rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","$localhome/mount/*","$localhome/VM/*","/mnt/*","/media/*","/lost+found","/swapfile"} / $remotelocation
--- backupusb.sh
+++ backupusb.sh
@@ -4,7 +4,7 @@
 # path:       ~/coding/shell/backupusb.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-17 16:57:03
+# date:       2019-11-21 01:34:44
 
 backupname="morpheus"
 mountpoint="/mnt/$backupname"
@@ -18,7 +18,7 @@ sudo mount $usbdevice $mountpoint
 
 # create folder and backup / to USB-Disk (for testing rsync option --dry-run)
 sudo mkdir -p $remotelocation
-sudo rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","$localhome/mount/*","$localhome/VM/*","/mnt/*","/media/*","/lost+found"} / $remotelocation
+sudo rsync -aAXv --delete --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","$localhome/mount/*","$localhome/VM/*","/mnt/*","/media/*","/lost+found","/swapfile"} / $remotelocation
 
 # unmount and delete folder
 sudo umount $mountpoint
