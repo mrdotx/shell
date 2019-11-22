ff859ba1dc3f33880575a3ffaeb10c2e8d294b99
mrdotx <klassiker@gmx.de>
2019-11-22 01:06:00 +0100

    add bar toggle

--- polybar.sh
+++ polybar.sh
@@ -4,24 +4,33 @@
 # path:       ~/coding/shell/polybar.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-13 12:46:20
+# date:       2019-11-22 00:26:26
+
+# toggle for polybar bars
+if [ "$(pgrep -xf "polybar i3slim")" ]; then
+    bar="i3"
+elif [ "$(pgrep -xf "polybar i3")" ]; then
+    bar="i3slim"
+else
+    bar="i3slim"
+fi
 
 # terminate already running bar instances
 killall -q polybar
 
 # wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -x polybar >/dev/null; do sleep 0.1; done
 
 # launch polybar
 for m in $(polybar -m | tail -1 | sed -e 's/:.*$//g'); do
-    MONITOR=$m polybar i3 &
+    MONITOR=$m polybar $bar &
 done
 
 # launch polybar on multiple monitors
 #if type "xrandr"; then
 #    for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-#        MONITOR=$m polybar i3 &
+#        MONITOR=$m polybar $bar &
 #    done
 #else
-#    polybar --reload i3 &
+#    polybar --reload $bar &
 #fi
