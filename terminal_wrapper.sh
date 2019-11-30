104b4f004c29643cb0bca7f198103b2770c8b6bd
mrdotx <klassiker@gmx.de>
2019-11-30 16:26:24 +0100

    change keypress

--- terminal_wrapper.sh
+++ terminal_wrapper.sh
@@ -4,10 +4,16 @@
 # path:       ~/coding/shell/terminal_wrapper.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-07 22:20:59
+# date:       2019-11-30 16:24:22
 
 # execute command in new terminal window
 if [ $# -eq 0 ]; then "${SHELL:-bash}"; else "$@"; fi
-echo ""
-echo "The command exited with status $?. Press Enter to close $TERMINAL."
-read -r
+echo; echo "The command exited with status $?."
+
+# wait for keypress
+while true; do
+    read -sn1 -p "Press (q)uit or (c)lose to exit this window..." keypress; echo
+    case "$keypress" in
+        Q|q|C|c) exit 0
+    esac
+done
