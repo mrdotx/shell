dca3af34879117dc279f616bcb42cf8b615187f7
mrdotx <klassiker@gmx.de>
2019-11-30 21:22:58 +0100

    add color

--- terminal_wrapper.sh
+++ terminal_wrapper.sh
@@ -4,15 +4,26 @@
 # path:       ~/coding/shell/terminal_wrapper.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-11-30 18:45:31
+# date:       2019-11-30 21:22:36
+
+# color variables
+#black=$(tput setaf 0)
+red=$(tput setaf 1)
+#green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+#blue=$(tput setaf 4)
+#magenta=$(tput setaf 5)
+#cyan=$(tput setaf 6)
+#white=$(tput setaf 7)
+reset=$(tput sgr0)
 
 # execute command in new terminal window
 if [ $# -eq 0 ]; then "${SHELL:-bash}"; else "$@"; fi
-echo; echo "The command exited with status $?."
+echo; echo "    - The command exited with status ${yellow}$?${reset}. -"
 
 # wait for keypress
 while true; do
-    read -sn1 -p "Press [q]uit or [c]lose to exit this window..." keypress; echo
+    read -rsn1 -p "Press [${red}q${reset}]uit or [${red}c${reset}]lose to exit this window..." keypress; echo
     case "$keypress" in
         Q|q|C|c) exit 0
     esac
