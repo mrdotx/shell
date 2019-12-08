29aebe0b8a4622062991b61fa100a4834d5062ab
mrdotx <klassiker@gmx.de>
2019-12-08 20:38:57 +0100

    bugfix first run

--- urxvtc.sh
+++ urxvtc.sh
@@ -3,11 +3,12 @@
 # path:       ~/coding/shell/urxvtc.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-12-01 14:41:49
+# date:       2019-12-08 20:38:53
 
 # start urxvt daemon if not running and open urxvt client
-urxvtc "$@"
-if [ $? -eq 2 ]; then
+if [ "$(pgrep -x urxvtd)" ]; then
+    urxvtc "$@"
+else
     urxvtd -q -o -f
     urxvtc "$@"
 fi
