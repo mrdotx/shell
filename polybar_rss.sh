c581ab70b8ce82610333f71ed3a55deb0e642070
mrdotx <klassiker@gmx.de>
2019-12-21 16:22:54 +0100

    change color

--- polybar_rss.sh
+++ polybar_rss.sh
@@ -3,10 +3,10 @@
 # path:       ~/projects/shell/polybar_rss.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-12-21 14:56:54
+# date:       2019-12-21 16:21:55
 
 # exit if newsboat is running
-pgrep -x newsboat > /dev/null 2>&1 && polybar-msg hook module/rss 2 > /dev/null 2>&1 && exit
+pgrep -x newsboat > /dev/null 2>&1 && polybar-msg hook module/rss 1 > /dev/null 2>&1 && exit
 
 if ping -c1 -W1 -q 1.1.1.1 > /dev/null 2>&1; then
     newsboat -x reload && newsboat -q -X > /dev/null 2>&1 && polybar-msg hook module/rss 3 > /dev/null 2>&1
