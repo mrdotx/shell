eb8b2c4fabe5e989710658706dec1fc85b2333cf
mrdotx <klassiker@gmx.de>
2019-12-26 18:54:15 +0100

    parameter changed

--- surf.sh
+++ surf.sh
@@ -3,30 +3,30 @@
 # path:       ~/projects/shell/surf.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-12-21 15:01:20
+# date:       2019-12-26 18:29:49
 
 xidfile="/tmp/tabbed-surf.xid"
 uri=""
 
 if [ "$#" -gt 0 ];
 then
-	uri="$1"
+    uri="$1"
 fi
 
 runtabbed() {
-	tabbed -dn tabbed-surf -r 2 surf -e '' "$uri" >"$xidfile" \
-		2>/dev/null &
+    tabbed -cdn tabbed-surf -r 2 surf -e '' "$uri" >"$xidfile" \
+        2>/dev/null &
 }
 
 if [ ! -r "$xidfile" ];
 then
-	runtabbed
+    runtabbed
 else
-	xid=$(cat "$xidfile")
-	if xprop -id "$xid" >/dev/null 2>&1;
-	then
-		surf -e "$xid" "$uri" >/dev/null 2>&1 &
-	else
+    xid=$(cat "$xidfile")
+    if xprop -id "$xid" >/dev/null 2>&1;
+    then
+        surf -e "$xid" "$uri" >/dev/null 2>&1 &
+    else
         runtabbed
-	fi
+    fi
 fi
