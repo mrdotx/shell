d5b35f0d207b3ff48207ec70bf6142646938460a
mrdotx <klassiker@gmx.de>
2020-01-09 12:20:37 +0100

    info added

--- wlan.sh
+++ wlan.sh
@@ -3,7 +3,31 @@
 # path:       ~/projects/shell/wlan.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2019-12-21 15:02:50
+# date:       2020-01-09 12:16:28
 
-# refresh wifi networks and open network manager
-nmcli device wifi list && nmtui
+script=$(basename "$0")
+help="$script [--help/-h] -- script for networkmanager
+  Usage:
+    $script [-nm/-info]
+
+  Settings:
+    [-nm]   = opens networkmanager
+    [-info] = shows available wlans
+
+  Examples:
+    $script -nm
+    $script -info"
+
+case "$1" in
+    -info)
+        watch -n5 nmcli device wifi list
+        ;;
+    -nm)
+        # refresh wifi networks and open network manager
+        nmcli device wifi list && nmtui
+        ;;
+    *)
+        echo "$help"
+        exit 0
+        ;;
+esac
