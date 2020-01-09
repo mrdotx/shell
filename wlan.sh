cc1cc57c453e3e9509e5273d2f094627c79dec16
mrdotx <klassiker@gmx.de>
2020-01-09 12:39:21 +0100

    deatils added

--- wlan.sh
+++ wlan.sh
@@ -3,28 +3,33 @@
 # path:       ~/projects/shell/wlan.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-01-09 12:16:28
+# date:       2020-01-09 12:39:16
 
 script=$(basename "$0")
 help="$script [--help/-h] -- script for networkmanager
   Usage:
-    $script [-nm/-info]
+    $script [-nm/-info/-detail]
 
   Settings:
-    [-nm]   = opens networkmanager
-    [-info] = shows available wlans
+    [-nm]     = opens networkmanager
+    [-info]   = shows available wlans updated every 5 seconds
+    [-detail] = shows available wlans
 
   Examples:
     $script -nm
-    $script -info"
+    $script -info
+    $script -detail"
 
 case "$1" in
-    -info)
-        watch -n5 nmcli device wifi list
-        ;;
     -nm)
         # refresh wifi networks and open network manager
         nmcli device wifi list && nmtui
+        ;;
+    -info)
+        watch -n5 nmcli dev wifi
+        ;;
+    -detail)
+        nmcli -m multiline -f ALL dev wifi
         ;;
     *)
         echo "$help"
