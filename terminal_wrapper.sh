4dc07db69a349a60380c61aed176b85ef93d04dd
mrdotx <klassiker@gmx.de>
2020-01-15 20:12:31 +0100

    esc exit added

--- terminal_wrapper.sh
+++ terminal_wrapper.sh
@@ -3,7 +3,7 @@
 # path:       ~/projects/shell/terminal_wrapper.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-01-13T12:17:13+0100
+# date:       2020-01-15T20:11:54+0100
 
 # color variables
 #black=$(tput setaf 0)
@@ -28,10 +28,10 @@ cmd_stat="The command exited with ${yellow}status $?${reset}. "
 
 # wait for keypress
 while true; do
-    read -rsn1 -p "${cmd_stat}Press [${green}q${reset}]${green}uit${reset} to exit this window or [${green}s${reset}]${green}hell${reset} to run $SHELL..." keypress
+    read -rsn1 -p "${cmd_stat}Press ${green}ESC${reset}, [${green}q${reset}]${green}uit${reset} to exit this window or [${green}s${reset}]${green}hell${reset} to run $SHELL..." keypress
     echo
     case "$keypress" in
-        q|Q) exit 0 ;;
+        q|Q|$'\x1B') exit 0 ;;
         s|S) $SHELL && exit 0;;
         *) cmd_stat=""
     esac
