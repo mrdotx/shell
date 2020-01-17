1ef239f7efbd8bf434d5627ab165d151477463c7
mrdotx <klassiker@gmx.de>
2020-01-17 15:54:51 +0100

    typos corrected

--- terminal_wrapper.sh
+++ terminal_wrapper.sh
@@ -3,7 +3,7 @@
 # path:       ~/projects/shell/terminal_wrapper.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-01-17T15:06:36+0100
+# date:       2020-01-17T15:52:59+0100
 
 # color variables
 #black=$(tput setaf 0)
@@ -24,15 +24,18 @@ else
 fi
 echo
 
-cmd_stat="The command exited with ${yellow}status $?${reset}. "
+stat="The command exited with ${yellow}status $?${reset}.
+"
+keys="Press ${green}ESC${reset}, [${green}q${reset}]${green}uit${reset} \
+to exit this window or [${green}s${reset}]${green}hell${reset} to run $SHELL..."
 
-# wait for keypress
+# wait for key
 while true; do
-    read -rsn1 -p "${cmd_stat}Press ${green}ESC${reset}, [${green}q${reset}]${green}uit${reset} to exit this window or [${green}s${reset}]${green}hell${reset} to run $SHELL..." keypress
+    read -rsN1 -p "${stat}${keys}" key
     echo
-    case "$keypress" in
+    case "$key" in
         q|Q|$'\x1B') exit 0 ;;
         s|S) $SHELL && exit 0;;
-        *) cmd_stat=""
+        *) stat=""
     esac
 done
