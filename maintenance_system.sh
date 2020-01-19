f4f9b4e6da14eb33b2fe2b8f4902c7fb58ab00d4
mrdotx <klassiker@gmx.de>
2020-01-19 10:42:58 +0100

    cache delete handling changed

--- maintenance_system.sh
+++ maintenance_system.sh
@@ -3,40 +3,40 @@
 # path:       ~/projects/shell/maintenance_system.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-01-13T12:15:04+0100
+# date:       2020-01-19T10:41:05+0100
 
-# color variables
-#black=$(tput setaf 0)
-#red=$(tput setaf 1)
-#green=$(tput setaf 2)
-#yellow=$(tput setaf 3)
-#blue=$(tput setaf 4)
-magenta=$(tput setaf 5)
-#cyan=$(tput setaf 6)
-#white=$(tput setaf 7)
-reset=$(tput sgr0)
-
-# purge cache
-echo "[${magenta}cache${reset}] purge all .cache files that have not been accessed in 100 days"
-find "$HOME/.cache/" -type f -atime +100 -delete
-
-# python history
-echo "[${magenta}python${reset}] remove white space from end of line"
+echo "python history"
+echo "  1. remove white space the from end of line"
 sed -i "s/ *$//" "$HOME/.python_history"
 
-echo "[${magenta}python${reset}] sort and remove duplicates"
+echo "  2. sort and remove duplicates"
 sort -u "$HOME/.python_history" | sort -o "$HOME/.python_history"
 
-# bash history
-echo "[${magenta}bash${reset}] remove white space from end of line"
+echo
+echo "bash history"
+echo "  1. remove white space from the end of line"
 sed -i "s/ *$//" "$HOME/.bash_history"
 
-echo "[${magenta}bash${reset}] sort and remove duplicates"
+echo "  2. sort and remove duplicates"
 sort -u "$HOME/.bash_history" | sort -o "$HOME/.bash_history"
 
-# zsh history
-echo "[${magenta}zsh${reset}] remove white space from end of line"
+echo
+echo "zsh history"
+echo "  1. remove white space from the end of line"
 sed -i "s/ *$//" "$HOME/.zsh_history"
 
-echo "[${magenta}zsh${reset}] sort and remove duplicates"
+echo "  2. sort and remove duplicates"
 sort -t ";" -k 2 -u "$HOME/.zsh_history" | sort -o "$HOME/.zsh_history"
+
+echo
+echo ".cache files"
+echo "  files that have not been accessed in 100 days"
+while true; do
+    printf "\r%s" "  press [enter] to show files or to delete cache [y]es/[n]o: " && read -r "key"
+    echo
+    case "$key" in
+        y|Y) find "$HOME/.cache/" -type f -atime +100 -delete && exit 0;;
+        n|N) exit 0;;
+        *) find "$HOME/.cache/" -type f -atime +100 ;;
+    esac
+done
