215a9efdaed3362dcddcab3c1d4aabba3519212e
mrdotx <klassiker@gmx.de>
2020-01-21 14:36:28 +0100

    automatic modus added

--- i3_tiling.sh
+++ i3_tiling.sh
@@ -3,14 +3,19 @@
 # path:       ~/projects/shell/i3_tiling.sh
 # user:       klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-01-21T11:00:21+0100
+# date:       2020-01-21T14:28:28+0100
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script for optimal tiling i3 focused window
   Usage:
     $script [command]
 
+  Settings:
+    [command] = open application in new window. without command, the script
+                runs in background and tile the windows automatic
+
   Examples:
+    $script
     $script firefox
     $script $TERMINAL"
 
@@ -30,11 +35,27 @@ split() {
     fi
 }
 
-if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ $# -eq 0 ]; then
+if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
     echo "$help"
     exit 0
-else
+elif [ $# -ne 0 ]; then
     dim \
         && split \
         && "$@" &
+else
+    if [ "$(pgrep -f i3_tiling.sh | wc -l)" -gt 2 ]; then
+        notify-send -i "$HOME/projects/shell/icons/i3.png" "i3" "optimal automatic tilings off"
+        pkill -f i3_tiling.sh
+    else
+        notify-send -i "$HOME/projects/shell/icons/i3.png" "i3" "optimal automatic tilings on"
+        wd=$(xdotool getwindowfocus getwindowpid)
+        while true
+        do
+            if [ ! "$(xdotool getwindowfocus getwindowpid)" = "$wd" ]; then
+                dim \
+                    && split
+            fi
+            sleep 1
+        done
+    fi
 fi
