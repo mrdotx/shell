8968a07bffdcc5479aa730c27f7cd66e714fd860
mrdotx <klassiker@gmx.de>
2020-05-05 14:34:38 +0200

    small changes

--- pdf_shrink.sh
+++ pdf_shrink.sh
@@ -3,31 +3,31 @@
 # path:       /home/klassiker/.local/share/repos/shell/pdf_shrink.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-04-29T11:16:03+0200
+# date:       2020-05-05T14:34:24+0200
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script to shrink pdf files
   Usage:
-    $script [pdf setting] [pdf filename]
+    $script [setting] [filename]
 
   Settings:
-    [pdf setting]  = name of the compression format
-      screen       = (72 dpi images)
-      ebook        = (150 dpi images)
-      printer      = (300 dpi images)
-      prepress     = (300 dpi images, color preserving)
-    [pdf filename] = name of the file to be compress
+    [setting]   = name of the compression format
+      screen    = (72 dpi images)
+      ebook     = (150 dpi images)
+      printer   = (300 dpi images)
+      prepress  = (300 dpi images, color preserving)
+    [filename]  = name of the file to be compress
 
   Example:
     $script screen document.pdf"
 
-pdf_setting=$1
-pdf_file=$2
-pdf_name=$(basename "$2" .pdf)
+setting=$1
+file=$2
+name=$(basename "$2" .pdf)
 
 if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ -z "$2" ] || [ $# -eq 0 ]; then
     printf "%s\n" "$help"
     exit 0
 else
-    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/"$pdf_setting" -dNOPAUSE -dBATCH -sOutputFile="$pdf_name"-compressed.pdf "$pdf_file"
+    gs -sDEVICE=pdfwrite -dCompatibilityLevel=1.4 -dPDFSETTINGS=/"$setting" -dNOPAUSE -dBATCH -sOutputFile="$name"-compressed.pdf "$file"
 fi
