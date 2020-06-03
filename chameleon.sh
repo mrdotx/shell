0970f5f9e92e5bf94321216f4fa0a9e6de18ebef
mrdotx <klassiker@gmx.de>
2020-06-03 18:54:09 +0200

    small fix

--- chameleon.sh
+++ chameleon.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/shell/chameleon.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-05-23T20:46:49+0200
+# date:       2020-06-03T18:52:30+0200
 
 color=$(chameleon)
 preview="/tmp/chameleon_preview.png"
@@ -11,7 +11,7 @@ preview="/tmp/chameleon_preview.png"
 convert xc:"$color" -resize 50 "$preview"
 
 [ -n "$color" ] \
-    && notify-send -i "$preview" "chameleon [$color]" "$color copied to\nclipboard and primary" \
+    && notify-send -i "$preview" "chameleon [$color]" "$color copied to clipboard and primary" \
     && printf "%s\n" "$color" \
         | xsel -b \
     && printf "%s\n" "$color" \
