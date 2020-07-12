225207efec29b7a71caea0078412fa42237ff476
mrdotx <klassiker@gmx.de>
2020-07-12 17:41:15 +0200

    option packages without dependencies added

--- yay-fzf.sh
+++ yay-fzf.sh
@@ -3,23 +3,25 @@
 # path:       /home/klassiker/.local/share/repos/shell/yay-fzf.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-06-28T08:48:45+0200
+# date:       2020-07-12T17:38:37+0200
 
 script=$(basename "$0")
 help="$script [-h/--help] -- script to install/remove packages with yay and fzf
   Usage:
-    $script [-s/-r/-e/-a]
+    $script [-s/-r/-e/-d/-a]
 
   Settings:
     [-s] = install packages from pacman or aur
     [-r] = remove installed packages from pacman or aur
     [-e] = remove explicit installed packages from pacman or aur
+    [-d] = remove installed packages without dependencies from pacman or aur
     [-a] = remove installed packages from aur
 
   Examples:
     $script -s
     $script -r
     $script -e
+    $script -d
     $script -a"
 
 execute() {
@@ -43,6 +45,9 @@ case "$1" in
         ;;
     -a)
         execute "Qm" "Rsn"
+        ;;
+    -d)
+        execute "Qqt" "Rsn"
         ;;
     *)
         printf "%s\n" "$help"
