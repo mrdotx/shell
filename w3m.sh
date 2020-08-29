f93569c83a7c5872cd81e90561cdbbdb06476e98
mrdotx <klassiker@gmx.de>
2020-08-29 23:47:38 +0200

    initial upload

---
+++ w3m.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# path:       /home/klassiker/.local/share/repos/shell/w3m.sh
+# author:     klassiker [mrdotx]
+# github:     https://github.com/mrdotx/surf
+# date:       2020-08-29T23:36:19+0200
+
+$TERMINAL -e w3m "$@" &
