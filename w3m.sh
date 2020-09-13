b30d767a524178e83e2539a0f499a8257c56631a
mrdotx <klassiker@gmx.de>
2020-09-13 18:34:55 +0200

    readable-cli added

--- w3m.sh
+++ w3m.sh
@@ -2,52 +2,68 @@
 
 # path:       /home/klassiker/.local/share/repos/shell/w3m.sh
 # author:     klassiker [mrdotx]
-# github:     https://github.com/mrdotx/surf
-# date:       2020-09-10T22:05:02+0200
+# github:     https://github.com/mrdotx/shell
+# date:       2020-09-13T18:30:09+0200
 
 script=$(basename "$0")
 help="$script [-h/--help] -- wrapper script to start w3m
   Usage:
-    $script [--tabbed]
+    $script [--readable/readable_window/--tabbed]
+
+    without settings the url opens in new terminal window
 
   Settings:
-    [--tabbed] = start w3m in suckless tabbed tool
+    [--readable]        = make the html content radable with readability-cli
+                          (Mozilla's Readability library)
+    [--readable_window] = same as readable in new window
+    [--tabbed]          = start w3m in suckless tabbed tool
 
   Examples:
     $script
-    $script duckduckgo.com
-    $script --tabbed duckduckgo.com"
+    $script suckless.org
+    $script --readable suckless.org
+    $script --readable_window suckless.org
+    $script --tabbed suckless.org"
 
-if [ "$1" = "-h" ] || [ "$1" = "--help" ]; then
-    printf "%s\n" "$help"
-elif [ "$1" = "--tabbed" ]; then
-    options="$2"
+case "$1" in
+    -h | --help)
+        printf "%s\n" "$help"
+        ;;
+    --readable)
+        readable "$2" | w3m -T text/html
+        ;;
+    --readable_window)
+        readable "$2" > "/tmp/readable.html"
+        $TERMINAL -e w3m "/tmp/readable.html" &
+        ;;
+    --tabbed)
+        xidfile="/tmp/w3m/tabbed-w3m.xid"
+        uri=""
 
-    xidfile="/tmp/w3m/tabbed-w3m.xid"
-    uri=""
+        [ ! -d /tmp/w3m ] \
+            && mkdir -p "/tmp/w3m"
 
-    [ ! -d /tmp/w3m ] \
-        && mkdir -p "/tmp/w3m"
+        [ -n "$2" ] \
+            && uri="$2"
 
-    [ -n "$options" ] \
-        && uri="$options"
+        runtabbed() {
+            tabbed -cdn tabbed-w3m -r 2 "$TERMINAL" -w '' -e w3m "$uri" >"$xidfile" 2>/dev/null &
+        }
 
-    runtabbed() {
-        tabbed -cdn tabbed-w3m -r 2 "$TERMINAL" -w '' -e w3m "$uri" >"$xidfile" 2>/dev/null &
-    }
-
-    if [ ! -r "$xidfile" ];
-    then
-        runtabbed
-    else
-        xid=$(cat "$xidfile")
-        if xprop -id "$xid" >/dev/null 2>&1;
+        if [ ! -r "$xidfile" ];
         then
-            "$TERMINAL" -w "$xid" -e w3m "$uri" >/dev/null 2>&1 &
-        else
             runtabbed
+        else
+            xid=$(cat "$xidfile")
+            if xprop -id "$xid" >/dev/null 2>&1;
+            then
+                "$TERMINAL" -w "$xid" -e w3m "$uri" >/dev/null 2>&1 &
+            else
+                runtabbed
+            fi
         fi
-    fi
-else
-    $TERMINAL -e w3m "$@" &
-fi
+        ;;
+    *)
+        $TERMINAL -e w3m "$@" &
+        ;;
+esac
