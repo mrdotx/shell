ab0c87708ae6f6d5e80dc3e7302c38e535a0689b
mrdotx <klassiker@gmx.de>
2020-09-26 17:34:18 +0200

    initial upload

---
+++ screenlayout/philips.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# path:       /home/klassiker/.local/share/repos/shell/screenlayout/philips.sh
+# author:     klassiker [mrdotx]
+# github:     https://github.com/mrdotx/shell
+# date:       2020-09-26T17:32:41+0200
+
+xrandr --output eDP1 --primary --mode 1920x1080 --pos 0x1080 --rotate normal --output DP1 --off --output HDMI1 --off --output HDMI2 --mode 1920x1080 --pos 0x0 --rotate normal --output VIRTUAL1 --off
