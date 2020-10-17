18edfa0dac807daa2cb8daec57e9902593c69683
mrdotx <klassiker@gmx.de>
2020-10-17 20:05:37 +0200

    copy hex value to primary removed

--- chameleon.sh
+++ chameleon.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/shell/chameleon.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-06-08T12:52:22+0200
+# date:       2020-10-17T20:02:33+0200
 
 color=$(chameleon)
 preview="/tmp/chameleon_preview.png"
@@ -12,9 +12,7 @@ convert xc:"$color" -resize 50 "$preview"
 
 [ -n "$color" ] \
     && printf "%s\n" "$color" \
-        | xsel -b \
-    && printf "%s\n" "$color" \
-        | xsel \
-    && notify-send -i "$preview" "chameleon [$color]" "$color copied to clipboard and primary"
+        | xsel -i -b \
+    && notify-send -i "$preview" "chameleon [$color]" "$color copied to clipboard"
 
 rm -f "$preview"
