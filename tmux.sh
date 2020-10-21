7a04529ce66eb7858ec7e1fd767a9d350d5426b3
mrdotx <klassiker@gmx.de>
2020-10-21 10:50:35 +0200

    initial upload

---
+++ tmux.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# path:       /home/klassiker/.local/share/repos/shell/tmux.sh
+# author:     klassiker [mrdotx]
+# github:     https://github.com/mrdotx/shell
+# date:       2020-10-21T10:35:38+0200
+
+tmux_config="$HOME/.config/tmux/tmux.conf"
+tmux_session="$1"
+
+tmux_autostart() {
+    tmux -f "$tmux_config" new -d -s "$1" -n "shell"
+    # tmux neww -t "$1":8 -n "htop" "htop"
+    # tmux splitw -h "watch -n2 grep 'MHz' /proc/cpuinfo"
+    # tmux splitw -v -p 100 "watch -n2 sensors"
+    tmux neww -t "$1":9 -n "bpytop" "bpytop"
+    tmux selectw -t "$1":0
+    tmux -2 attach -d
+}
+
+tmux_attach() {
+    tmux attach -t "$1"
+}
+
+if [ -z "$TMUX" ]; then
+    tmux_attach "$tmux_session" \
+        || tmux_autostart "$tmux_session"
+fi
