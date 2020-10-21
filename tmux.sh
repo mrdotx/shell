9ad3404f5680c279174c6301b2ab0b90b07914b3
mrdotx <klassiker@gmx.de>
2020-10-21 20:38:26 +0200

    split window for autostart added

--- tmux.sh
+++ tmux.sh
@@ -3,27 +3,41 @@
 # path:       /home/klassiker/.local/share/repos/shell/tmux.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-10-21T12:18:17+0200
+# date:       2020-10-21T20:24:36+0200
 
-tmux_config="$HOME/.config/tmux/tmux.conf"
-tmux_session="${1:-mi}"
+config="$HOME/.config/tmux/tmux.conf"
+session="${1:-mi}"
 
-tmux_autostart() {
-    tmux -f "$tmux_config" new -d -s "$1" -n "shell"
-    # tmux neww -t "$1":8 -n "htop" "htop"
-    # tmux splitw -h "watch -n2 grep 'MHz' /proc/cpuinfo"
-    # tmux splitw -v -p 100 "watch -n2 sensors"
-    ! [ "$(pgrep -f "bpytop")" ] \
-        && tmux neww -t "$1":9 -n "bpytop" "bpytop"
-    tmux selectw -t "$1":0
-    tmux -2 attach -d
+tmux_neww() {
+    window=$1
+    shift
+    cmd="$*"
+    ! [ "$(pgrep -f "$cmd")" ] \
+        && tmux neww -t "$session":"$window" -n "$cmd" "$cmd"
+}
+
+tmux_splitw() {
+    window=$1
+    direction=$2
+    shift 2
+    cmd="$*"
+    ! [ "$(pgrep -f "$cmd")" ] \
+        && tmux splitw -t "$session":"$window" -"$direction" "$cmd"
 }
 
 tmux_attach() {
-    tmux attach -t "$1"
+    tmux attach -t "$session"
+}
+
+tmux_autostart() {
+    tmux -f "$config" new -d -s "$session" -n "shell"
+    tmux_neww 9 "bpytop"
+    # tmux_splitw 9 h "watch -n2 grep 'MHz' /proc/cpuinfo"
+    tmux selectw -t "$session":0
+    tmux -2 attach -d
 }
 
 if [ -z "$TMUX" ]; then
-    tmux_attach "$tmux_session" \
-        || tmux_autostart "$tmux_session"
+    tmux_attach \
+        || tmux_autostart
 fi
