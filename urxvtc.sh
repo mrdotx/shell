05cdbe9feb09eddcd82a41d7455a893eef828a0d
mrdotx <klassiker@gmx.de>
2020-11-14 11:34:03 +0100

    minor optimization

--- urxvtc.sh
+++ urxvtc.sh
@@ -3,9 +3,13 @@
 # path:       /home/klassiker/.local/share/repos/shell/urxvtc.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-11-10T20:23:53+0100
+# date:       2020-11-14T10:11:19+0100
 
-urxvtc "$@"
-[ $? -eq 2 ] \
-    && urxvtd -q -o -f \
-    && urxvtc "$@"
+daemon="urxvtd -q -o -f"
+
+if [ "$(pgrep -fx "$daemon")" ]; then
+    urxvtc "$@"
+else
+    $daemon
+    urxvtc "$@"
+fi
