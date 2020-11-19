97c892adc5588016817076aafc818571abba50a6
mrdotx <klassiker@gmx.de>
2020-11-19 11:52:42 +0100

    functions consolidated

--- xwallpaper.sh
+++ xwallpaper.sh
@@ -3,36 +3,44 @@
 # path:       /home/klassiker/.local/share/repos/shell/xwallpaper.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-11-18T19:34:11+0100
+# date:       2020-11-19T11:34:28+0100
 
-# set xresources uri
-set_uri() {
-    file="$HOME/.config/xorg/xwallpaper"
-    sed -i "/Xwallpaper.uri:/c\Xwallpaper.uri:     $1" "$file"
-    xrdb -merge "$HOME/.config/xorg/Xresources"
+config="$HOME/.config/xorg/xwallpaper"
+xresource="$HOME/.config/xorg/Xresources"
+
+# get random picture from directory
+rnd_pic() {
+    find "$1" -type f | shuf -n 1
+}
+
+# get/set xresource value
+xresource() {
+    case "$1" in
+        get_value)
+            printf "%s" "$(xrdb -query \
+                | grep Xwallpaper."$2": \
+                | cut -f2)"
+            ;;
+        set_value)
+            sed -i "/Xwallpaper.uri:/c\Xwallpaper.$2:         $3" "$config"
+            xrdb -merge "$xresource"
+            ;;
+    esac
 }
 
 if [ -d "$1" ]; then
-    uri="$(find "$1" -type f | shuf -n 1)"
-    set_uri "$1"
+    uri="$(rnd_pic "$1")"
+    xresource set_value uri "$1"
 elif [ -f "$1" ]; then
     uri="$1"
-    set_uri "$1"
+    xresource set_value uri "$1"
 else
-    # get xresources uri
-    uri=$(printf "%s" "$(xrdb -query \
-        | grep Xwallpaper.uri: \
-        | cut -f2)" \
-    )
+    uri="$(xresource get_value uri)"
 
     [ -d "$uri" ] \
-        && uri="$(find "$uri" -type f | shuf -n 1)"
+        && uri="$(rnd_pic "$uri")"
 fi
 
-# get xresources options
-options=$(printf "%s" "$(xrdb -query \
-    | grep Xwallpaper.options: \
-    | cut -f2)" \
-)
+options="$(xresource get_value options)"
 
 eval xwallpaper "$options" "$uri"
