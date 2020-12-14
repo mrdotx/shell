67dc8e062a4d271cd8b03e3d2c475884e5df1fee
mrdotx <klassiker@gmx.de>
2020-12-14 09:53:34 +0100

    monitor position changed

--- screenlayout/lg.sh
+++ screenlayout/lg.sh
@@ -3,7 +3,7 @@
 # path:       /home/klassiker/.local/share/repos/shell/screenlayout/lg.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-10-05T11:15:24+0200
+# date:       2020-12-13T12:13:58+0100
 
 xrandr \
     --output eDP1 --primary \
@@ -11,6 +11,7 @@ xrandr \
     --pos 0x1050 \
     --rotate normal \
     --output HDMI2 \
+    --right-of eDP1 \
     --mode 1680x1050 \
     --pos 120x0 \
     --rotate normal \
