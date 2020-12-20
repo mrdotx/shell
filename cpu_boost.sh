8d38a7557c6cf9b7fbf769434dc04e90f2b8141d
mrdotx <klassiker@gmx.de>
2020-12-20 10:03:58 +0100

    changed from tlp to cpufreqctl

--- cpu_boost.sh
+++ cpu_boost.sh
@@ -3,23 +3,59 @@
 # path:       /home/klassiker/.local/share/repos/shell/cpu_boost.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-10-18T11:22:48+0200
+# date:       2020-12-20T10:02:33+0100
 
 # auth can be something like sudo -A, doas -- or
 # nothing, depending on configuration requirements
 auth="doas"
-file="/etc/tlp.d/99-xiaomi-mi.conf"
 
-# toggle powersave <-> performance
-if grep -q "CPU_SCALING_GOVERNOR_ON_AC=performance" "$file"; then
-    $auth sed -i '/CPU_SCALING_GOVERNOR_ON_AC=performance/c\CPU_SCALING_GOVERNOR_ON_AC=powersave' "$file"
-    $auth sed -i '/CPU_SCALING_GOVERNOR_ON_BAT=performance/c\CPU_SCALING_GOVERNOR_ON_BAT=powersave' "$file"
-    printf "set scaling governor to powersave\n"
-else
-    $auth sed -i '/CPU_SCALING_GOVERNOR_ON_AC=powersave/c\CPU_SCALING_GOVERNOR_ON_AC=performance' "$file"
-    $auth sed -i '/CPU_SCALING_GOVERNOR_ON_BAT=powersave/c\CPU_SCALING_GOVERNOR_ON_BAT=performance' "$file"
-    printf "set scaling governor to performance\n"
-fi
+script=$(basename "$0")
+help="$script [-h/--help] -- script to toggle cpu indicators
+  Usage:
+    $script [--toggle/--status/--available]
 
-# restart tlp
-$auth tlp start >/dev/null 2>&1
+  Settings:
+    [--toggle] = switch between powersave and performance
+    [--status] = shows status of governor and epp
+    [--available] = shows available indicators of governor and epp
+
+  Examples:
+    $script --toggle
+    $script --status
+    $script --available"
+
+case "$1" in
+    --toggle)
+        status=$($auth cpufreqctl --governor)
+        # toggle powersave <-> performance
+        case $status in
+            performance*)
+                $auth cpufreqctl --governor --set=powersave
+                $auth cpufreqctl --epp --set=balance_power
+                ;;
+            powersave*)
+                $auth cpufreqctl --governor --set=performance
+                $auth cpufreqctl --epp --set=performance
+                ;;
+        esac
+        ;;
+    --status)
+        printf "%s\n   %s\n" \
+            ":: CPU governors" \
+            "$($auth cpufreqctl --governor)"
+        printf "%s\n   %s\n" \
+            ":: CPU energy/perfomance policies (EPP)" \
+            "$($auth cpufreqctl --epp)"
+        ;;
+    --available)
+        printf "%s\n   %s\n" \
+            ":: CPU governors" \
+            "$($auth cpufreqctl --governor --available)"
+        printf "%s\n   %s\n" \
+            ":: CPU energy/perfomance policies (EPP)" \
+            "$($auth cpufreqctl --epp --available)"
+        ;;
+    *)
+        printf "%s\n" "$help"
+        ;;
+esac
