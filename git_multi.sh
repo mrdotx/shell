f0741bf86020dc5c94b2e66ccd78c2dd62c0256a
mrdotx <klassiker@gmx.de>
2020-12-28 22:50:44 +0100

    typos corrected

--- git_multi.sh
+++ git_multi.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-# path:       /home/klassiker/.local/share/repos/shell/git_fetch.sh
+# path:       /home/klassiker/.local/share/repos/shell/git_multi.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-12-28T22:42:27+0100
+# date:       2020-12-28T22:49:37+0100
 
 config="
     $HOME/.local/share/repos
