3b886eb31e8279b13c2ae4bc2cdd1b7025477b98
mrdotx <klassiker@gmx.de>
2020-12-29 14:33:56 +0100

    help, folders as option and colors to output added

--- git_multi.sh
+++ git_multi.sh
@@ -3,20 +3,52 @@
 # path:       /home/klassiker/.local/share/repos/shell/git_multi.sh
 # author:     klassiker [mrdotx]
 # github:     https://github.com/mrdotx/shell
-# date:       2020-12-28T22:49:37+0100
+# date:       2020-12-29T13:23:05+0100
 
-config="
-    $HOME/.local/share/repos
-"
-options="${*:-status}"
+# config
+default="status"
+
+# color variables
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+reset=$(tput sgr0)
+
+# help
+script=$(basename "$0")
+help="$script [-h/--help] -- script to execute git command on multiple repositories
+  Usage:
+    $script [git option] [path-1] [path-n]
+
+  Settings:
+    [git option] = option like fetch origin or pull (default=$default)
+    [path]       = path to folder with n repositories
+
+  Examples:
+    $script
+    $script \"git fetch\" \"$HOME/.local/share/repos\"
+    $script \"pull\" \"\$HOME/.local/share/repos\" \"/srv/http\""
+
+case $1 in
+    -h | --help)
+        printf "%s\n" "$help"
+        exit 0
+        ;;
+    */*)
+        options="$default"
+        ;;
+    *)
+        options="${1:-$default}"
+        [ $# -ge 1 ] \
+            && shift
+esac
+config="$(printf "%s" "${*:-$(pwd)}" | sed 's/ /\n/g')"
 
 folder() {
     printf "%s\n" "$config" | {
         while IFS= read -r line; do
             [ -n "$line" ] \
-                && line=$(printf "%s/*/.git" "$line" \
-                    | sed 's/ //g') \
-                && locate "$line"
+                && find "$line" -name ".git" -maxdepth 2 |
+                    sort
         done
     }
 }
@@ -26,7 +58,12 @@ printf "%s\n" "$(folder)" | {
         [ -n "$line" ] \
             && line=$(printf "%s" "$line" \
                 | sed 's/ //g;s/\/.git//') \
-            && printf ":: git $options \"%s\"\n" "$line" \
+            && printf ":: %sgit %s %s\"%s\"%s\n" \
+                "$green" \
+                "$options" \
+                "$yellow" \
+                "$line" \
+                "$reset" \
             && cd "$line" \
             && eval git "$options"
     done
