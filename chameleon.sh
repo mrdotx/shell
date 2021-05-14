ea916b6c19fca9cf5fee350d490251d75ad92c22
mrdotx <klassiker@gmx.de>
2021-05-15 00:21:35 +0200

    added sleep workaround

--- chameleon.sh
+++ chameleon.sh
@@ -3,9 +3,13 @@
 # path:   /home/klassiker/.local/share/repos/shell/chameleon.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2021-01-15T13:58:39+0100
+# date:   2021-05-14T20:11:42+0200
 
-color=$(chameleon)
+color=$( \
+    # workaround (sleep -> https://github.com/i3/i3/issues/3298)
+    sleep .5 \
+    && chameleon \
+)
 preview="/tmp/chameleon_preview.png"
 
 convert xc:"$color" -resize 50 "$preview"
