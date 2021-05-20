671a95a1bc340568a8ec71ce610c726fa35803ac
mrdotx <klassiker@gmx.de>
2021-05-20 19:09:10 +0200

    structure simplified

--- urxvtc.sh
+++ urxvtc.sh
@@ -3,26 +3,20 @@
 # path:   /home/klassiker/.local/share/repos/shell/urxvtc.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2021-05-19T17:37:44+0200
-
-daemon="urxvtd -q -o -f"
-client="urxvtc"
-
-run() {
-    $1 -fx "$daemon" >/dev/null 2>&1
-}
+# date:   2021-05-20T18:52:27+0200
 
 case "$1" in
     --kill)
-        run "pgrep" \
-            && run "pkill"
+        urxvtc -k
         ;;
     *)
-        run "pgrep" \
-            && $client "$@" \
+        daemon="urxvtd -q -o -f"
+
+        pgrep -fx "$daemon" >/dev/null 2>&1 \
+            && urxvtc "$@" \
             && exit 0
 
-            $daemon
-            $client "$@"
+        $daemon
+        urxvtc "$@"
         ;;
 esac
