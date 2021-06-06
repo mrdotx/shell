bd1e4b3a208bc0a75b6ab7d175f9b0db7c32a8fe
mrdotx <klassiker@gmx.de>
2021-06-06 09:20:14 +0200

    minor optimizations

--- gpg_edit.sh
+++ gpg_edit.sh
@@ -3,21 +3,15 @@
 # path:   /home/klassiker/.local/share/repos/shell/gpg_edit.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2021-06-05T17:38:08+0200
+# date:   2021-06-05T20:16:35+0200
 
 edit="$EDITOR"
 working_directory="$(pwd)"
-
-if [ -z "$PASSWORD_STORE_DIR" ]; then
-    password_store="$HOME/.password_store"
-else
-    password_store="$PASSWORD_STORE_DIR"
-fi
+password_store="${PASSWORD_STORE_DIR-$HOME/.password-store}"
 
 if printf "%s" "$working_directory" | grep -q "^$password_store"; then
     path="$(printf "%s/\n" "$1" \
             | sed -e "s#$password_store/##" -e "s#\.gpg##")"
-
     pass edit "$path"
 else
     $edit "$1"
