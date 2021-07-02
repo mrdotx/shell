219d1a54cbce54575eeb9f6ad430657702b83585
mrdotx <klassiker@gmx.de>
2021-07-02 10:09:36 +0200

    parallel download added

--- git_clone.sh
+++ git_clone.sh
@@ -3,23 +3,28 @@
 # path:   /home/klassiker/.local/share/repos/shell/git_clone.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2021-07-02T09:26:04+0200
+# date:   2021-07-02T10:08:35+0200
+
+# config
+cn="users"
+name="mrdotx"
+procs=4
 
 # help
 script=$(basename "$0")
 help="$script [-h/--help] -- script to clone all repositories of a user or organization
   Usage:
-    $script [cn] [name] [page]
+    $script [cn] [name] [procs]
 
   Settings:
-    [cn]   = must be users or orgs (default=users)
-    [name] = must be a username or organisation (default=mrdotx)
-    [page] = page number to download (default=1)
-             the maximum page-size is 100 repositories per page
+    [cn]    = must be users or orgs (default=$cn)
+    [name]  = must be a username or organisation (default=$name)
+    [procs] = processes to parallel download the repositories (default=$procs)
 
   Examples:
     $script
-    $script users mrdotx 1"
+    $script $cn $name
+    $script $cn $name $procs"
 
 case $1 in
     -h | --help)
@@ -27,8 +32,8 @@ case $1 in
         exit 0
         ;;
     *)
-        curl -s "https://api.github.com/${1-users}/${2-mrdotx}/repos?page=${3-1}&per_page=100" \
+        curl -s "https://api.github.com/${1-$cn}/${2-$name}/repos?per_page=1000" \
             | grep 'git_url' \
             | cut -d "\"" -f 4 \
-            | xargs -L1 git clone
+            | xargs -P"${3-$procs}" -L1 git clone
 esac
