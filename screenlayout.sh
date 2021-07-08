9ddb6decca40057f4eba39f809c850b0f9c5ad13
mrdotx <klassiker@gmx.de>
2021-07-08 15:04:00 +0200

    primary and secondary monitor changed

--- screenlayout.sh
+++ screenlayout.sh
@@ -3,11 +3,11 @@
 # path:   /home/klassiker/.local/share/repos/shell/screenlayout.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2021-07-08T07:58:37+0200
+# date:   2021-07-08T14:56:26+0200
 
 # config
-primary="eDP1"
-secondary="HDMI2"
+primary="HDMI2"
+secondary="eDP1"
 
 # help
 script=$(basename "$0")
@@ -19,16 +19,17 @@ help="$script [-h/--help] -- script to set screenlayout
     [--list]    = list of defined settings for dmenu
     [parameter] = semicolon separated
                   1) primary mode (default: 1920x1080)
-                  2) primary position (default: 1920x0)
-                  3) secondary mode (default: 1920x1080)
-                  4) secondary position (default: 0x0)
-                  5) secondary rate (default: 74.99)
+                  2) primary position (default: 0x0)
+                  3) primary rate (default: 74.99)
+                  4) secondary mode (default: 1920x1080)
+                  5) secondary position (default: 1920x0)
+                  6) secondary rate (default: 60.00)
 
   Examples:
     $script
     $script --list
-    $script \"1920x1080;0x1080;1920x1080;0x0;74.99\"
-    $script \";;;;60\"
+    $script \"1920x1080;0x0;74.99;1920x1080;1920x0;60.00\"
+    $script \";;60.00;;;\"
 
   Config:
     primary = $primary
@@ -40,12 +41,12 @@ case "$1" in
         ;;
     --list)
         printf "%s\n" \
-            "1920x1080;1920x0;1920x1080;0x0;74.99" \
-            "1920x1080;1920x0;1920x1080;0x0;60" \
-            "1920x1080;0x1050;1680x1050;120x0;60"
+            "1920x1080;0x0;74.99;1920x1080;1920x0;60.00" \
+            "1920x1080;0x0;60.00;1920x1080;1920x0;60.00" \
+            "1920x1080;1680x1050;60.00;0x1050;120x0;60.00"
         ;;
     *)
-        # if extrenal monitor is disconnected use primary screen else use configuration
+        # if extrenal monitor is disconnected use internal screen else use configuration
         if xrandr | grep "$secondary disconnected"; then
             xrandr \
                 --output "$primary" --auto \
@@ -56,18 +57,20 @@ case "$1" in
         else
             pri_mode=$(printf "%s" "$1" | cut -d ';' -f1)
             pri_pos=$(printf "%s" "$1" | cut -d ';' -f2)
-            sec_mode=$(printf "%s" "$1" | cut -d ';' -f3)
-            sec_pos=$(printf "%s" "$1" | cut -d ';' -f4)
-            sec_rate=$(printf "%s" "$1" | cut -d ';' -f5)
+            pri_rate=$(printf "%s" "$1" | cut -d ';' -f3)
+            sec_mode=$(printf "%s" "$1" | cut -d ';' -f4)
+            sec_pos=$(printf "%s" "$1" | cut -d ';' -f5)
+            sec_rate=$(printf "%s" "$1" | cut -d ';' -f6)
 
             xrandr \
                 --output "$primary" --primary \
                 --mode "${pri_mode:-1920x1080}" \
-                --pos "${pri_pos:-1920x0}" \
+                --pos "${pri_pos:-0x0}" \
+                --rate "${pri_rate:-74.99}" \
                 --output "$secondary" \
                 --mode "${sec_mode:-1920x1080}" \
-                --pos "${sec_pos:-0x0}" \
-                --rate "${sec_rate:-74.99}" \
+                --pos "${sec_pos:-1920x0}" \
+                --rate "${sec_rate:-60.00}" \
                 --output DP1 --off \
                 --output HDMI1 --off \
                 --output VIRTUAL1 --off
