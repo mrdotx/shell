72bbb7a0888d9a80164fbada4000520be80390df
mrdotx <klassiker@gmx.de>
2021-07-10 10:46:18 +0200

    defaults changed

--- screenlayout.sh
+++ screenlayout.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/shell/screenlayout.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2021-07-08T18:26:23+0200
+# date:   2021-07-10T10:41:37+0200
 
 # config
 primary="HDMI2"
@@ -13,23 +13,23 @@ secondary="eDP1"
 script=$(basename "$0")
 help="$script [-h/--help] -- script to set screenlayout
   Usage:
-    $script [--list] [parameter]
+    $script [--defaults] [parameter]
 
   Settings:
-    [--list]    = list of defined settings for dmenu
-    [parameter] = semicolon separated
-                  1) primary mode (default: 1920x1080)
-                  2) primary position (default: 0x0)
-                  3) primary rate (default: 74.99)
-                  4) secondary mode (default: 1920x1080)
-                  5) secondary position (default: 1920x0)
-                  6) secondary rate (default: 60.00)
+    [--defaults] = list of default settings for dmenu
+    [parameter]  = semicolon separated
+                   1) primary mode (default: 1920x1080)
+                   2) primary position (default: 0x0)
+                   3) primary rate (default: 60)
+                   4) secondary mode (default: 1920x1080)
+                   5) secondary position (default: 1920x0)
+                   6) secondary rate (default: 60)
 
   Examples:
     $script
     $script --list
-    $script \"1920x1080;0x0;74.99;1920x1080;1920x0;60.00\"
-    $script \";;60.00;;;\"
+    $script \"1920x1080;0x0;75;1920x1080;1920x0;60\"
+    $script \";;75;;;\"
 
   Config:
     primary = $primary
@@ -56,11 +56,11 @@ dual_monitor() {
         --output "$primary" --primary \
         --mode "${pri_mode:-1920x1080}" \
         --pos "${pri_pos:-0x0}" \
-        --rate "${pri_rate:-74.99}" \
+        --rate "${pri_rate:-60}" \
         --output "$secondary" \
         --mode "${sec_mode:-1920x1080}" \
         --pos "${sec_pos:-1920x0}" \
-        --rate "${sec_rate:-60.00}" \
+        --rate "${sec_rate:-60}" \
         --output DP1 --off \
         --output HDMI1 --off \
         --output VIRTUAL1 --off
@@ -70,11 +70,11 @@ case "$1" in
     -h | --help)
         printf "%s\n" "$help"
         ;;
-    --list)
+    --defaults)
         printf "%s\n" \
-            "1920x1080;0x0;74.99;1920x1080;1920x0;60.00" \
-            "1920x1080;0x0;60.00;1920x1080;1920x0;60.00" \
-            "1920x1080;1680x1050;60.00;0x1050;120x0;60.00"
+            "1920x1080;0x0;60;1920x1080;1920x0;60" \
+            "1920x1080;0x0;75;1920x1080;1920x0;60" \
+            "1920x1080;1680x1050;60;0x1050;120x0;60"
         ;;
     *)
         if xrandr | grep "$primary disconnected"; then
