05f134193b19aa485c5d7a819c160deb9233ac83
mrdotx <klassiker@gmx.de>
2021-09-12 09:22:48 +0200

    added random picture based on saved file/path from xresources

--- wallpaper.sh
+++ wallpaper.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/shell/wallpaper.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2021-09-08T20:37:00+0200
+# date:   2021-09-12T09:10:04+0200
 
 xresource="$HOME/.config/xorg/Xresources"
 config="$HOME/.config/xorg/modules/wallpaper"
@@ -11,17 +11,20 @@ config="$HOME/.config/xorg/modules/wallpaper"
 script=$(basename "$0")
 help="$script [-h/--help] -- wrapper script to set wallpaper
   Usage:
-    $script <path/file>
+    $script [--random] <path/file>
 
   Settings:
     without given settings, load wallpaper from xresources
-    [folder] = set random picture from folder as wallpaper
-    [file]   = set picture as wallpaper
+    [folder]   = set random picture from folder as wallpaper and save folder
+                 path to xresources
+    [file]     = set picture as wallpaper and save filename to xresources
+    [--random] = set random picture based on saved <path/file> from xresources
 
   Examples:
     $script
     $script $HOME/Pictures/Wallpaper
     $script $HOME/Pictures/Wallpaper/Dark_Blue/pcb.jpg
+    $script --random
 
   Config:
     xresource = $xresource
@@ -46,6 +49,16 @@ xresource() {
     esac
 }
 
+random_uri() {
+    uri="$(xresource get_value uri)"
+    if [ -d "$uri" ]; then \
+        uri="$(rnd_pic "$uri")"
+    else
+        uri="$(rnd_pic "$(dirname "$uri")")"
+    fi
+    printf "%s" "$uri"
+}
+
 process_uri() {
     if [ -d "$1" ]; then
         uri="$(rnd_pic "$1")"
@@ -64,6 +77,9 @@ process_uri() {
 case "$1" in
     -h | --help)
         printf "%s\n" "$help"
+        ;;
+    --random)
+        $(xresource get_value tool) "$(random_uri)" >/dev/null 2>&1
         ;;
     *)
         $(xresource get_value tool) "$(process_uri "$1")" >/dev/null 2>&1
