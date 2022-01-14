54cbec250e92894502eefbb3ac474d05707ffd33
mrdotx <klassiker@gmx.de>
2022-01-14 14:50:46 +0100

    remote location changed

--- backup.sh
+++ backup.sh
@@ -3,13 +3,13 @@
 # path:   /home/klassiker/.local/share/repos/shell/backup.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2022-01-12T20:41:46+0100
+# date:   2022-01-14T14:04:09+0100
 
 # auth can be something like sudo -A, doas -- or nothing,
 # depending on configuration requirements
 auth="$EXEC_AS_USER"
 backup_name=$(hostname)
-remote_location="prometheus:/home/alarm/backup/$backup_name/"
+remote_location="pi2:/home/alarm/backup/$backup_name/"
 
 printf "\n:: backup / to remote location\n"
 # for testing rsync option --dry-run
