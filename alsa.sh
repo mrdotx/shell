02c1f89761c74cf40db0cd8b9cce588638d820ab
mrdotx <klassiker@gmx.de>
2022-04-17 16:47:38 +0200

    initial upload

---
+++ alsa.sh
@@ -0,0 +1,113 @@
+#!/bin/sh
+
+# path:   /home/klassiker/.local/share/repos/shell/alsa.sh
+# author: klassiker [mrdotx]
+# github: https://github.com/mrdotx/dmenu
+# date:   2022-04-17T16:43:53+0200
+
+# speed up script by not using unicode
+LC_ALL=C
+LANG=C
+
+# config
+config_path="$HOME/.config/alsa"
+config_file="asoundrc"
+analog_filter="analog"
+message_title="Volume"
+
+script=$(basename "$0")
+help="$script [-h/--help] -- script to change alsa audio output
+  Usage:
+    $script [-inc/-dec/-abs/-mute] [percent]
+
+  Settings:
+    [-inc]    = increase in percent (0-100)
+    [-dec]    = decrease in percent (0-100)
+    [-abs]    = absolute volume in percent (0-100)
+    [percent] = how much percent to increase/decrease the volume
+    [-mute]   = mute volume
+
+  Examples:
+    $script -inc 5
+    $script -dec 5
+    $script -abs 36
+    $script -mute"
+
+notification() {
+    volume="$(amixer get "$1" \
+        | tail -1 \
+        | cut -d'[' -f2 \
+        | sed 's/%]*//' \
+    )"
+
+    if amixer get "$2" | tail -1 | grep "\[off\]" >/dev/null; then
+        volume=0 \
+        volume_indicator="MUTE"
+    else
+        volume=$((volume /= ${3:-1}))
+        volume=$((volume *= ${3:-1}))
+        volume_indicator="$volume"
+    fi
+
+    notify-send \
+        -u low  \
+        -t 2000 \
+        -i "dialog-information" \
+        "$message_title $volume_indicator" \
+        -h string:x-canonical-private-synchronous:"$message_title" \
+        -h int:value:"$volume"
+}
+
+get_analog() {
+    card=$(grep -m1 "card" "$config_path/$config_file" \
+        | sed 's/^    //' \
+    )
+
+    if ! aplay -l \
+        | grep -m1 -i -e "^$card.*$analog_filter" > /dev/null 2>&1; then
+        device_mixer="PCM"
+        device_mute="IEC958"
+    else
+        device_mixer="Master"
+        device_mute="Master"
+    fi
+}
+
+set_volume() {
+    if [ "$#" -eq 2 ] \
+        && [ "$2" -ge 0 ] > /dev/null 2>&1 \
+        && [ "$2" -le 100 ] > /dev/null 2>&1; then
+            amixer -q set $device_mixer "$2%$1"
+            notification "$device_mixer" "$device_mute" "$2"
+    else
+        printf "%s\n" "$help"
+        exit 1
+    fi
+}
+
+case "$1" in
+    -h | --help)
+        printf "%s\n" "$help"
+        ;;
+    -inc)
+        get_analog
+        set_volume "+" "$2"
+        ;;
+    -dec)
+        get_analog
+        set_volume "-" "$2"
+        ;;
+    -abs)
+        get_analog
+        set_volume "" "$2"
+        ;;
+    -mute)
+        get_analog
+        amixer -q set $device_mute toggle
+        notification "$device_mixer" "$device_mute"
+        ;;
+    *)
+        printf "%s\n" "$help"
+        exit 1
+        ;;
+esac
---
+++ backlight.sh
@@ -0,0 +1,102 @@
+#!/bin/sh
+
+# path:   /home/klassiker/.local/share/repos/shell/backlight.sh
+# author: klassiker [mrdotx]
+# github: https://github.com/mrdotx/backlight
+# date:   2022-04-17T16:43:48+0200
+
+# speed up script by not using unicode
+LC_ALL=C
+LANG=C
+
+# config
+auth="${EXEC_AS_USER:-sudo}"
+message_title="Brightness"
+
+script=$(basename "$0")
+help="$script [-h/--help] -- script to change backlight
+  Usage:
+    $script [-inc/-dec] [percent]
+
+  Settings:
+    [-inc]    = increase in percent (0-100)
+    [-dec]    = decrease in percent (0-100)
+    [percent] = how much percent to increase/decrease the brightness
+
+  Examples:
+    $script -inc 5
+    $script -dec 5"
+
+backlight_dir=$( \
+    find /sys/class/backlight -type l \
+        | head -n1 \
+)
+
+notification() {
+    if [ "$1" -ge 100 ]; then
+        brightness=$(($1 / 100))
+        brightness=$(($2 / brightness))
+    else
+        brightness=$((100 / $1))
+        brightness=$(($2 * brightness))
+    fi
+    brightness=$((brightness /= $3))
+    brightness=$((brightness *= $3))
+
+    notify-send \
+        -u low  \
+        -t 2000 \
+        -i "dialog-information" \
+        "$message_title $brightness" \
+        -h string:x-canonical-private-synchronous:"$message_title" \
+        -h int:value:"$brightness"
+}
+
+brightness() {
+    if [ "$1" -ge 0 ] \
+        && [ "$1" -le 100 ]; then
+            max=$(cat "$backlight_dir/max_brightness")
+            actual=$(cat "$backlight_dir/actual_brightness")
+
+            percent=$((100 / $1))
+            factor=$((max / percent))
+
+            [ $factor -eq 0 ] \
+                && factor=1
+    else
+        printf "%s\n" "$help"
+        exit 1
+    fi
+}
+
+set_brightness() {
+    $auth sh -c "printf \"%s\" \"$1\" > \"$backlight_dir/brightness\""
+}
+
+case "$1" in
+    -h | --help)
+        printf "%s\n" "$help"
+        ;;
+    -inc)
+        brightness "$2"
+        value=$((actual + factor))
+        if [ $value -ge "$max" ]; then
+            value=$max
+        fi
+        set_brightness "$value"
+        notification "$max" "$value" "$2"
+        ;;
+    -dec)
+        brightness "$2"
+        value=$((actual - factor))
+        if [ $value -le 0 ]; then
+            value=0
+        fi
+        set_brightness "$value"
+        notification "$max" "$value" "$2"
+        ;;
+    *)
+        printf "%s\n" "$help"
+        exit 1
+        ;;
+esac
