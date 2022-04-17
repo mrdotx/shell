d5a60cfd7242c819e98a43763a592f18026d9020
mrdotx <klassiker@gmx.de>
2022-04-17 18:41:37 +0200

    minor structural changes

--- touchpad_toggle.sh
+++ touchpad_toggle.sh
@@ -3,19 +3,20 @@
 # path:   /home/klassiker/.local/share/repos/shell/touchpad_toggle.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2021-10-15T18:08:21+0200
+# date:   2022-04-17T18:41:23+0200
 
 # speed up script by not using unicode
 LC_ALL=C
 LANG=C
 
 device="$(xinput list \
-        | grep -P '(?<= )[\w\s:]*(?i)(touchpad|synaptics)(?-i).*?(?=\s*id)' -o \
-        | head -n1 \
-    )"
+    | grep -P '(?<= )[\w\s:]*(?i)(touchpad|synaptics)(?-i).*?(?=\s*id)' -o \
+    | head -n1 \
+)"
 
-if [ "$(xinput list-props "$device" | grep -P ".*Device Enabled.*\K.(?=$)" -o)" = "1" ]; then
-    xinput disable "$device"
+if [ "$(xinput list-props "$device" \
+    | grep -P ".*Device Enabled.*\K.(?=$)" -o)" = "1" ]; then
+        xinput disable "$device"
 else
     xinput enable "$device"
 fi
