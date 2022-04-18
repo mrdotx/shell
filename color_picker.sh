eca4f90a40342aa9360863724ad02f8c779c0362
mrdotx <klassiker@gmx.de>
2022-04-18 16:14:09 +0200

    workaround readded for color picker don't start in i3

--- color_picker.sh
+++ color_picker.sh
@@ -3,13 +3,17 @@
 # path:   /home/klassiker/.local/share/repos/shell/color_picker.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2022-04-18T15:48:30+0200
+# date:   2022-04-18T16:12:52+0200
 
 # speed up script by not using unicode
 LC_ALL=C
 LANG=C
 
-color=$(xcolor)
+color=$( \
+    # workaround (sleep -> https://github.com/i3/i3/issues/3298)
+    sleep .5 \
+        && xcolor \
+)
 
 preview="$(mktemp -t color_picker_preview.XXXXXX.png)"
 
