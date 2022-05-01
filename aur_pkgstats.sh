d953f4afa796f1977a6154474f83cde6febfd4da
mrdotx <klassiker@gmx.de>
2022-05-01 18:53:59 +0200

    extension moved to config

--- aur_pkgstats.sh
+++ aur_pkgstats.sh
@@ -3,13 +3,14 @@
 # path:   /home/klassiker/.local/share/repos/shell/aur_pkgstats.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2022-05-01T18:14:27+0200
+# date:   2022-05-01T18:53:57+0200
 
 # config
 url="https://pkgstats.archlinux.de/api/packages"
 pkgs_dir="$HOME/.local/share/repos/aur"
 stats_dir="_pkgstats"
 destination="$pkgs_dir/$stats_dir"
+extension="json"
 
 get_stats() {
     mkdir -p "$destination"
@@ -18,7 +19,6 @@ get_stats() {
     startmonth=$(printf "%s" "$data" \
         | awk -F '\"startMonth\":' '{print $2}' \
         | cut -d',' -f1)
-    extension="json"
 
     printf "%s" "$data" > "$destination/$1-$startmonth.$extension"
     printf "%s-%s.%s\n" "$1" "$startmonth" "$extension"
