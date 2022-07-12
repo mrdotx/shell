93ae5b0a9e890263d7f1c633d164faddc0a7f6d0
mrdotx <klassiker@gmx.de>
2022-07-12 11:23:10 +0200

    wlan signal changed

--- status.sh
+++ status.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/shell/status.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2022-04-24T08:04:29+0200
+# date:   2022-07-12T11:00:49+0200
 
 # speed up script by using standard c
 LC_ALL=C
@@ -22,11 +22,11 @@ help="$script [-h/--help] -- script to show system information
     $script -l"
 
 kb_mb() {
-    printf "%.0fM\n" "$(($1*1000/1024))e-3"
+    printf "%.0fM\n" "$(($1 * 1000 / 1024))e-3"
 }
 
 kb_gb() {
-    printf "%.2fG\n" "$(($1*1000/1024/1024))e-3"
+    printf "%.2fG\n" "$(($1 * 1000 / 1024 / 1024))e-3"
 }
 
 kernel() {
@@ -36,7 +36,7 @@ kernel() {
 cpu() {
     cores="$(grep -c "^processor" /proc/cpuinfo)"
     cpu_usage="$(ps aux | awk 'BEGIN {sum=0} {sum+=$3}; END {print sum*1000}')"
-    cpu_usage="$(printf "%.0f\n" "$((cpu_usage/${cores:-1}))e-3")"
+    cpu_usage="$(printf "%.0f\n" "$((cpu_usage / ${cores:-1}))e-3")"
 
     if test -d /sys/class/thermal/thermal_zone0/temp; then
         cpu_temp="$(cut -c "1-2" /sys/class/thermal/thermal_zone0/temp)C"
@@ -50,8 +50,8 @@ ram() {
     ram_total="$(free | awk 'NR==2 { printf "%s",$2; }')"
     ram_together="$(free | awk 'NR==2 { printf "%s",$5; }')"
     ram="$(free | awk 'NR==2 { printf "%s",$3; }')"
-    ram="$(printf "%.0f\n" "$(((ram+ram_together)))")"
-    ram_usage="$(printf "%.0f\n" "$(((ram)/((ram_total)/100)))")"
+    ram="$(printf "%.0f\n" "$((ram + ram_together))")"
+    ram_usage="$(printf "%.0f\n" "$((ram / ram_total / 100))")"
     if [ "$ram" -le 1048576 ]; then
         printf "%s/%s [%s%%]" "$(kb_mb "$ram")" "$(kb_gb "$ram_total")" "$ram_usage"
     else
@@ -62,7 +62,7 @@ ram() {
 swap() {
     swap_total="$(free | awk 'NR==3 { printf "%s",$2; }')"
     swap="$(free | awk 'NR==3 { printf "%s",$3; }')"
-    swap_usage="$(printf "%.0f\n" "$(((swap)/((swap_total)/100)))")"
+    swap_usage="$(printf "%.0f\n" "$((swap / swap_total / 100))")"
     if [ "$swap" -le 1048576 ]; then
         printf "%s/%s [%s%%]" "$(kb_mb "$swap")" "$(kb_gb "$swap_total")" "$swap_usage"
     else
@@ -80,7 +80,7 @@ space() {
 wlan() {
     wlan="$(iwgetid -r)"
     wlan_signal="$(iwconfig "$1" | grep -i Link | cut -c "24-25")"
-    wlan_signal="$(printf "%.0f\n" "$((wlan_signal*100/70))")"
+    wlan_signal="$(printf "%.0f\n" "$wlan_signal")"
     printf "%s [%s%%]" "$wlan" "$wlan_signal"
 }
 
