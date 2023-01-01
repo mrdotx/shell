762bd359f057a0de00be51ce1492782246bc64d0
mrdotx <klassiker@gmx.de>
2023-01-01 11:34:53 +0100

    search in current folder per default added

--- delete_metafiles.sh
+++ delete_metafiles.sh
@@ -3,16 +3,18 @@
 # path:   /home/klassiker/.local/share/repos/shell/delete_metafiles.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2022-05-30T12:37:30+0200
+# date:   2023-01-01T11:34:20+0100
 
 [ ! "$(id -u)" = 0 ] \
     && printf "this script needs root privileges to run\n" \
     && exit 1
 
+# default search in the current folder
+folder="${1:-.}"
 output=$(mktemp -t delete_metafiles.XXXXXX)
 
 # files to delete
-find /home \( \
+find "$folder" \( \
     -name ".DS_Store" \
     -o -name "._*" \
     -o -name ".AppleDouble" \
