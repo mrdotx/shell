f10b0deb41db705ec8908ed673f32bd80d518faa
mrdotx <klassiker@gmx.de>
2023-01-05 18:56:21 +0100

    minor optimizations

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/shell/backup.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2023-01-05T13:24:06+0100
+# date:   2023-01-05T18:56:16+0100
 
 # auth can be something like sudo -A, doas -- or nothing,
 # depending on configuration requirements
@@ -22,67 +22,46 @@ usb_devices="
     /dev/disk/by-uuid/4ceb144a-db56-4211-96f6-147e1beab18a
     /dev/disk/by-uuid/2bdffcfb-b365-4321-a64b-5ffce2f1c211
 " # 4ceb -> 3.5, 2bdf -> 2.5
+local_hostname="$(hostname)"
 
-backup_to_ssh() {
-    # set remote location and rsync options by hostname
-    case "$1" in
-        m75q)
-            remote="pi2:/home/alarm/backup/$1/"
-            options="$rsync_options \
-                --exclude='/home/klassiker/.local/share/cloud' \
-                --exclude='/home/klassiker/.local/vms'"
-            ;;
-        mi)
-            remote="pi:/home/alarm/backup/$1/"
-            options="$rsync_options \
-                --exclude='/home/klassiker/.local/share/cloud' \
-                --exclude='/home/klassiker/.local/vms'"
-            ;;
-        pi)
-            remote="pi2:/home/alarm/backup/$1/"
-            options="$rsync_options \
-                --exclude='/home/alarm/backup'"
-            ;;
-        pi2)
-            remote="pi:/home/alarm/backup/$1/"
-            options="$rsync_options \
-                --exclude='/home/alarm/backup'"
-            ;;
-    esac
-
-    printf "\n:: backup / to %s\n" "$remote"
-    eval "$auth rsync $options / $remote"
-}
-
+# helper functions
 mount_usb() {
-    mount_point="/mnt/$1"
+    mount_point="/mnt/$local_hostname"
 
     printf ":: create and mount backup folder %s\n" "$mount_point"
     $auth mkdir -p "$mount_point"
-    $auth mount "$2" "$mount_point"
+    $auth mount "$1" "$mount_point"
 }
 
 unmount_usb() {
-    mount_point="/mnt/$1"
+    mount_point="/mnt/$local_hostname"
 
     printf "\n:: unmount and delete folder %s\n" "$mount_point"
     $auth umount "$mount_point"
     $auth find "$mount_point" -empty -type d -delete
 }
 
+backup_to_usb() {
+    remote="/mnt/$local_hostname/Backup/$local_hostname"
+
+    printf "\n:: create folder and backup / to %s\n" "$remote"
+    $auth mkdir -p "$remote"
+    eval "$auth rsync $rsync_options / $remote"
+}
+
 backup_from_ssh() {
-    local="/mnt/$1/Backup/$2"
+    local="/mnt/$local_hostname/Backup/$1"
 
     # set remote location and rsync options by hostname
-    case "$2" in
+    case "$1" in
         pi)
-            remote="$2:/"
+            remote="$1:/"
             options="$rsync_options \
                 --exclude='/home/alarm/backup' \
                 --rsync-path='$auth rsync'"
             ;;
         pi2)
-            remote="$2:/"
+            remote="$1:/"
             options="$rsync_options \
                 --exclude='/home/alarm/backup' \
                 --rsync-path='$auth rsync'"
@@ -94,22 +73,46 @@ backup_from_ssh() {
     eval "$auth rsync $options $remote $local"
 }
 
-backup_to_usb() {
-    remote="/mnt/$1/Backup/$1"
+backup_to_ssh() {
+    # set remote location and rsync options by hostname
+    case "$local_hostname" in
+        m75q)
+            remote="pi2:/home/alarm/backup/$local_hostname/"
+            options="$rsync_options \
+                --exclude='/home/klassiker/.local/share/cloud' \
+                --exclude='/home/klassiker/.local/vms'"
+            ;;
+        mi)
+            remote="pi:/home/alarm/backup/$local_hostname/"
+            options="$rsync_options \
+                --exclude='/home/klassiker/.local/share/cloud' \
+                --exclude='/home/klassiker/.local/vms'"
+            ;;
+        pi)
+            remote="pi2:/home/alarm/backup/$local_hostname/"
+            options="$rsync_options \
+                --exclude='/home/alarm/backup'"
+            ;;
+        pi2)
+            remote="pi:/home/alarm/backup/$local_hostname/"
+            options="$rsync_options \
+                --exclude='/home/alarm/backup'"
+            ;;
+    esac
 
-    printf "\n:: create folder and backup / to %s\n" "$remote"
-    $auth mkdir -p "$remote"
-    eval "$auth rsync $rsync_options / $remote"
+    printf "\n:: backup / to %s\n" "$remote"
+    eval "$auth rsync $options / $remote"
 }
 
+# main
 for device in $usb_devices; do
     [ -h "$device" ] \
-        && mount_usb "$(hostname)" "$device" \
-        && backup_to_usb "$(hostname)" \
-        && backup_from_ssh "$(hostname)" "pi" \
-        && backup_from_ssh "$(hostname)" "pi2" \
-        && unmount_usb "$(hostname)" \
+        && mount_usb "$device" \
+        && backup_to_usb \
+        && backup_from_ssh "pi" \
+        && backup_from_ssh "pi2" \
+        && unmount_usb \
         && exit 0
 done
 
-backup_to_ssh "$(hostname)"
+backup_to_ssh
