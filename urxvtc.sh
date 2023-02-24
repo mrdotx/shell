552e3b6fb32d6b6c9eb6b4ee94d382bd89bf946e
mrdotx <klassiker@gmx.de>
2023-02-24 19:44:05 +0100

    minor changes

--- urxvtc.sh
+++ urxvtc.sh
@@ -3,20 +3,16 @@
 # path:   /home/klassiker/.local/share/repos/shell/urxvtc.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2021-05-20T18:52:27+0200
+# date:   2023-02-24T18:02:37+0100
 
 case "$1" in
     --kill)
         urxvtc -k
         ;;
     *)
-        daemon="urxvtd -q -o -f"
-
-        pgrep -fx "$daemon" >/dev/null 2>&1 \
-            && urxvtc "$@" \
-            && exit 0
-
-        $daemon
         urxvtc "$@"
+        [ $? -eq 2 ] \
+            && urxvtd -q -o -f \
+            && urxvtc "$@"
         ;;
 esac
