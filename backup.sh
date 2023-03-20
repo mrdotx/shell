78e2f1acaa74b5823a78447246099697bd1f0385
mrdotx <klassiker@gmx.de>
2023-03-20 20:18:23 +0100

    mi added to backup

--- backup.sh
+++ backup.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/shell/backup.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2023-03-15T20:35:02+0100
+# date:   2023-03-20T20:18:17+0100
 
 # auth can be something like sudo -A, doas -- or nothing,
 # depending on configuration requirements
@@ -44,7 +44,7 @@ unmount_usb() {
     $auth find "$mount_point" -empty -type d -delete
 }
 
-backup_to_usb() {
+backup_local() {
     remote="/mnt/$local_hostname/hosts/$local_hostname"
 
     printf "\n:: create folder and backup / to %s\n" "$remote"
@@ -52,7 +52,7 @@ backup_to_usb() {
     eval "$auth rsync $rsync_options / $remote"
 }
 
-backup_from_ssh() {
+backup_remote() {
     local="/mnt/$local_hostname/hosts/$1"
 
     # set remote location and rsync options by hostname
@@ -63,18 +63,33 @@ backup_from_ssh() {
                 --exclude='/srv/http/download' \
                 --rsync-path='$auth rsync'"
             ;;
+        mi)
+            remote="$1:/"
+            options="$rsync_options \
+                --rsync-path='$auth rsync'"
+            ;;
     esac
 
     printf "\n:: create folder and backup %s to %s\n" "$remote" "$local"
     $auth mkdir -p "$local"
-    eval "$auth rsync $options $remote $local"
+    ssh -q "$1" exit
+    case $? in
+        0)
+            eval "$auth rsync $options $remote $local"
+            ;;
+        *)
+            printf "connect to %s failed, please check if ssh is enabled!\n" \
+                "$1"
+            ;;
+    esac
 }
 
 # main
 [ -h "$usb_partlabel" ] \
     && mount_usb "$usb_partlabel" \
-    && backup_to_usb \
-    && backup_from_ssh "m625q" \
+    && backup_local \
+    && backup_remote "m625q" \
+    && backup_remote "mi" \
     && unmount_usb \
     && exit 0
 
