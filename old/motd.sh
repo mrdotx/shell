b6ad0b081f8fccab71b5ae97422728f4dcc1d021
mrdotx <klassiker@gmx.de>
2023-03-27 11:30:11 +0200

    funtionality restored and minor optimizations

--- old/motd.sh
+++ old/motd.sh
@@ -3,183 +3,192 @@
 # path:   /home/klassiker/.local/share/repos/shell/old/motd.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2021-01-15T13:57:40+0100
+# date:   2023-03-27T11:21:26+0200
 
-# functions {{{
+me=$(whoami)
+
+# layout functions
 color() {
-    echo "\e[$1m$2\e[0m"
+    printf "\e[%sm%s\e[0m" "$1" "$2"
 }
 
 extend() {
     str="$1"
-    (( spaces=60-${#1} ))
+    spaces=$((61-${#1}))
     while [ $spaces -gt 0 ]; do
         str="$str "
-        (( spaces=spaces-1 ))
+        spaces=$((spaces-1))
     done
-    echo "$str"
+    printf "%s" "$str"
 }
 
 center() {
     str="$1"
-    (( spacesLeft=(78-${#1})/2 ))
-    (( spacesRight=78-spacesLeft-${#1} ))
+    width=${2:-78}
+    spacesLeft=$(((width-${#1})/2))
+    spacesRight=$((width-spacesLeft-${#1}))
     while [ $spacesLeft -gt 0 ]; do
         str=" $str"
-        (( spacesLeft=spacesLeft-1 ))
+        spacesLeft=$((spacesLeft-1))
     done
 
     while [ $spacesRight -gt 0 ]; do
         str="$str "
-        (( spacesRight=spacesRight-1 ))
+        spacesRight=$((spacesRight-1))
     done
 
-    echo "$str"
+    printf "%s" "$str"
 }
 
-logoCenter() {
-    str="$1"
-    (( spacesLeft=(60-${#1})/2 ))
-    (( spacesRight=60-spacesLeft-${#1} ))
-    while [ $spacesLeft -gt 0 ]; do
-        str=" $str"
-        (( spacesLeft=spacesLeft-1 ))
-    done
-
-    while [ $spacesRight -gt 0 ]; do
-        str="$str "
-        (( spacesRight=spacesRight-1 ))
-    done
-
-    echo "$str"
-}
-
+# uptime
 sec2time() {
     input=$1
 
     if [ "$input" -lt 60 ]; then
-        echo "$input seconds"
+        printf "%s seconds" "$input"
     else
-        (( days=input/86400 ))
-        (( input=input%86400 ))
-        (( hours=input/3600 ))
-        (( input=input%3600 ))
-        (( mins=input/60 ))
+        days=$((input/86400))
+        input=$((input%86400))
+        hours=$((input/3600))
+        input=$((input%3600))
+        mins=$((input/60))
 
-        daysPlural="s"
-        hoursPlural="s"
-        minsPlural="s"
+        [ $days -ne 1 ] \
+            && daysPlural="s"
 
-        if [ $days -eq 1 ]; then
-            daysPlural=""
-        fi
+        [ $hours -ne 1 ] \
+            && hoursPlural="s"
 
-        if [ $hours -eq 1 ]; then
-            hoursPlural=""
-        fi
+        [ $mins -ne 1 ] \
+            && minsPlural="s"
 
-        if [ $mins -eq 1 ]; then
-            minsPlural=""
-        fi
-
-        echo "$days day$daysPlural, $hours hour$hoursPlural, $mins minute$minsPlural"
+        printf "%s day%s, %s hour%s, %s minute%s" \
+            "$days" \
+            "$daysPlural" \
+            "$hours" \
+            "$hoursPlural" \
+            "$mins" \
+            "$minsPlural"
     fi
 }
-# }}}
 
-# colors {{{
+uptime_long() {
+    printf "%s [%s]" \
+        "$(sec2time "$(cut -d "." -f 1 /proc/uptime)")" \
+        "$(date -d "@""$(grep btime /proc/stat | cut -d " " -f 2)" +"%d.%m.%Y %H:%M:%S")"
+}
+
+# tty login
+tty_login() {
+    read -r loginFrom loginIP loginMonth loginDay loginTime loginYear << EOF
+        $(last "$me" -w -F -d | awk 'NR==2 { print $2,$3,$5,$6,$7,$8 }')
+EOF
+
+    loginDate="$(date -d "$loginMonth $loginDay $loginTime $loginYear" -Ins)"
+
+    case $loginDate in
+        -)
+            loginDate=$loginIP
+            loginIP=$loginFrom
+            ;;
+    esac
+
+    case $loginDate in
+        *T*)
+            login="$(date -d "$loginDate" +"%a, %d %B %Y, %T") [$loginIP]"
+            ;;
+        *)
+            # not enough logins
+            login="None"
+            ;;
+    esac
+
+    printf "%s" "$login"
+}
+
+# memory
+memory() {
+    case $1 in
+        swap)
+            free -m \
+                | awk 'NR==3 { printf "Total: %sMB, Used: %sMB, Free: %sMB",$2,$3,$4; }'
+            ;;
+        *)
+            free -m \
+                | awk 'NR==2 { printf "Total: %sMB, Used: %sMB, Free: %sMB, Cached: %sMB",$2,$3,$4,$7; }'
+            ;;
+    esac
+}
+# space
+space() {
+    df -h "$1" \
+        | awk 'NR==2 { printf "Total: %sB, Used: %sB, Free: %sB, Utilized: %s",$2,$3,$4,$5; }'
+}
+
+# cpu
+cpu() {
+    printf "CPU: %sMHz, TEMP: %sºC" \
+        "$(($(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq) / 1000))" \
+        "$(cut -c "1-2" /sys/class/hwmon/hwmon0/temp1_input)"
+}
+
+# alive
+alive() {
+    ping -c1 -W1 -q "$1" >/dev/null 2>&1
+    case $? in
+        0)
+            printf "online"
+            ;;
+        *)
+            printf "offline"
+            ;;
+    esac
+}
+
+# colors
 borderColor="0;34"
 headerXtogoColor="1;36"
 greetingsColor="1;32"
 statsLabelColor="1;33"
-# }}}
 
-# border {{{
+# border
 borderLine="━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
 borderTopLine=$(color $borderColor "┏$borderLine┓")
 borderBottomLine=$(color $borderColor "┗$borderLine┛")
 borderBar=$(color $borderColor "┃")
 borderEmptyLine="$borderBar                                                                              $borderBar"
-# }}}
 
-# greetings {{{
-me=$(whoami)
-greetings="$(color $greetingsColor "$(logoCenter "Willkommen zurück, $me!")")"
-greetingsDate="$(color $greetingsColor "$(logoCenter "$(date +"%a, %d %B %Y, %T")")")"
-greetingsKernel="$(color $greetingsColor "$(logoCenter "$(uname -r -m)")")"
-greetingsKernelVersion="$(color $greetingsColor "$(logoCenter "$(uname -v)")")"
-# }}}
+# greetings
+greetings="$(color $greetingsColor "$(center "Welcome back, $me!" 55)")"
+greetingsDate="$(color $greetingsColor "$(center "$(date +"%a, %d %B %Y, %T")" 55)")"
+greetingsKernel="$(color $greetingsColor "$(center "$(uname -r -m)" 55)")"
+greetingsKernelVersion="$(color $greetingsColor "$(center "$(uname -v)" 55)")"
 
-# header {{{
+# header
 header="$borderTopLine\n"
-header="$header$borderBar$(color $headerXtogoColor "   ___        ___                                                             ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "   \  \      /  /  __                                .___                     ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "    \  \    /  / _/  |_  ____   ____   ____        __| _/____                 ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "     \  \  /  /  \   __\/  _ \ / ___\ /  _ \      / __ |/ __ \                ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "      \  \/  /    |  | (  <_> ) /_/  >  <_> )    / /_/ \  ___/                ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "       >    <     |__|  \____/\___  / \____/  /\ \____ |\___  >               ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "      /  /\  \               /_____/          \/      \/    \/                ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "     /  /  \  \                                                               ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "    /  /    \  \  $greetings")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "   /__/      \_ \ $greetingsDate")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "               \/ $greetingsKernel")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "                  $greetingsKernelVersion")$borderBar\n"
+header="$header$borderBar$(color $headerXtogoColor "                   .___      __                                               ")$borderBar\n"
+header="$header$borderBar$(color $headerXtogoColor "   ____________  __| _/_____/  |____  ___                                     ")$borderBar\n"
+header="$header$borderBar$(color $headerXtogoColor "  /     \_  __ \/ __ |/  _ \   __\  \/  /                                     ")$borderBar\n"
+header="$header$borderBar$(color $headerXtogoColor " |  Y Y  \  | \/ /_/ (  <_> )  |  >    <                                      ")$borderBar\n"
+header="$header$borderBar$(color $headerXtogoColor " |__|_|  /__|  \____ |\____/|__| /__/\_ \                                     ")$borderBar\n"
+header="$header$borderBar$(color $headerXtogoColor "       \/           \/                 \/                                     ")$borderBar\n"
+header="$header$borderEmptyLine\n"
+header="$header$borderBar$(color $headerXtogoColor "                       $greetings")$borderBar\n"
+header="$header$borderBar$(color $headerXtogoColor "                       $greetingsDate")$borderBar\n"
+header="$header$borderBar$(color $headerXtogoColor "                       $greetingsKernel")$borderBar\n"
+header="$header$borderBar$(color $headerXtogoColor "                       $greetingsKernelVersion")$borderBar\n"
 header="$header$borderBottomLine"
-# }}}
 
-# system information {{{
-read -r loginFrom loginIP loginMonth loginDay loginTime loginYear <<<"$(last "$me" -w -F -d | awk 'NR==2 { print $2,$3,$5,$6,$7,$8 }')"
-loginDate="$(date -d "$loginMonth $loginDay $loginTime $loginYear" -Ins)"
-# }}}
-
-# tty login {{{
-if [[ $loginDate == - ]]; then
-    loginDate=$loginIP
-    loginIP=$loginFrom
-fi
-
-if [[ $loginDate == *T* ]]; then
-    login="$(date -d "$loginDate" +"%a, %d %B %Y, %T") [$loginIP]"
-else
-    # not enough logins
-    login="None"
-fi
-# }}}
-
-# labels {{{
-label1="$(extend "$login")"
-label1="$borderBar  $(color $statsLabelColor "Last Login....:") $label1$borderBar"
-
-uptime="$(sec2time "$(cut -d "." -f 1 /proc/uptime)")"
-uptime="$uptime [$(date -d "@""$(grep btime /proc/stat | cut -d " " -f 2)" +"%d-%m-%Y %H:%M:%S")]"
-
-label2="$(extend "$uptime")"
-label2="$borderBar  $(color $statsLabelColor "Uptime........:") $label2$borderBar"
-
-label3="$(extend "$(free -m | awk 'NR==2 { printf "Total: %sMB, Used: %sMB, Free: %sMB, Cached: %sMB",$2,$3,$4,$7; }')")"
-label3="$borderBar  $(color $statsLabelColor "Memory........:") $label3$borderBar"
-
-label4="$(extend "$(free -m | awk 'NR==3 { printf "Total: %sMB, Used: %sMB, Free: %sMB",$2,$3,$4; }')")"
-label4="$borderBar  $(color $statsLabelColor "Swap..........:") $label4$borderBar"
-
-label5="$(extend "$(df -h ~ | awk 'NR==2 { printf "Total: %sB, Used: %sB, Free: %sB, Utilized: %s",$2,$3,$4,$5; }')")"
-label5="$borderBar  $(color $statsLabelColor "Home space....:") $label5$borderBar"
-
-label6="$(extend "$(df -h /dev/sda1 | awk 'NR==2 { printf "Total: %sB, Used: %sB, Free: %sB, Utilized: %s",$2,$3,$4,$5; }')")"
-label6="$borderBar  $(color $statsLabelColor "EFI space.....:") $label6$borderBar"
-
-label7="$(extend "CPU: $(echo $(($(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq) / 1000)))MHz, Temp: $(cat /sys/class/thermal/thermal_zone0/temp | cut -c "1-2")ºC")"
-label7="$borderBar  $(color $statsLabelColor "System:.......:") $label7$borderBar"
-
-#label8="$(extend "$($HOME/repos/shell/snippets/hera.sh status)")"
-#label8="$borderBar  $(color $statsLabelColor "NAS...........:") $label8$borderBar"
-
-#label9="$(extend "$($HOME/repos/shell/snippets/host_status.sh elysion) [elysion]                                           ")"
-#label9="$borderBar  $(color $statsLabelColor "BACKUP........:") $label12$borderBar"
+# labels
+label1="$borderBar $(color $statsLabelColor "Last Login....:") $(extend "$(tty_login)")$borderBar"
+label2="$borderBar $(color $statsLabelColor "Uptime........:") $(extend "$(uptime_long)")$borderBar"
+label3="$borderBar $(color $statsLabelColor "Memory........:") $(extend "$(memory)")$borderBar"
+label4="$borderBar $(color $statsLabelColor "Swap..........:") $(extend "$(memory swap)")$borderBar"
+label5="$borderBar $(color $statsLabelColor "Boot..........:") $(extend "$(space /boot)")$borderBar"
+label6="$borderBar $(color $statsLabelColor "Home..........:") $(extend "$(space /home)")$borderBar"
+label7="$borderBar $(color $statsLabelColor "System:.......:") $(extend "$(cpu)")$borderBar"
 
 stats="$label1\n$label2\n$label3\n$label4\n$label5\n$label6\n$label7"
-# }}}
 
-# print motd {{{
+# main
 echo -e "$header\n$borderTopLine\n$stats\n$borderBottomLine"
-# }}}
