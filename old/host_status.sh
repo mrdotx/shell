23a9e88a1726dbf3f124ff1a38f64bbfaa50df6b
mrdotx <klassiker@gmx.de>
2023-03-27 11:26:54 +0200

    minor optimizations

--- old/host_status.sh
+++ old/host_status.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/shell/old/host_status.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2021-01-15T13:57:24+0100
+# date:   2023-03-27T10:01:05+0200
 
 # color variables
 red=$(tput setaf 1)
@@ -11,8 +11,12 @@ green=$(tput setaf 2)
 reset=$(tput sgr0)
 
 # ping host and echo online or offline
-if ping -c 1 "$1" -w 1 >/dev/null 2>&1; then
-    printf "%sonline %s" "${green}" "${reset}"
-else
-    printf "%soffline%s" "${red}" "${reset}"
-fi
+ping -c1 -W1 -q "$1" >/dev/null 2>&1
+case $? in
+    0)
+        printf "%sonline%s\n" "$green" "$reset"
+        ;;
+    *)
+        printf "%soffline%s\n" "$red" "$reset"
+        ;;
+esac
