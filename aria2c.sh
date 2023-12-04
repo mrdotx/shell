6a3a696f573b2f305b1764c9f6ab02b937426b1f
mrdotx <klassiker@gmx.de>
2023-12-04 08:35:23 +0100

    add url fixed

--- aria2c.sh
+++ aria2c.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/shell/aria2c.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2023-11-07T18:16:49+0100
+# date:   2023-12-02T18:52:48+0100
 
 # config
 links_file="$HOME/Public/downloads/links.txt"
@@ -38,13 +38,14 @@ aria2c_stop() {
 
 aria2c_start() {
     aria2c \
-        -i "$links_file" \
+        --input-file="$links_file" \
         --save-session="$links_file"
 }
 
 add_url() {
     [ -s "$links_file" ] \
-        && printf "%s\n%s\n" "$(grep -v " gid=" "$2")" "$1" \
+        && links=$(grep -v " gid=" "$links_file") \
+        && printf "%s\n%s\n" "$links" "$1" \
             | tr -d '[:blank:]' \
             | sort -u > "$links_file" \
         && return 0
@@ -59,12 +60,12 @@ case "$1" in
         ;;
     -a | --add)
         [ -n "$2" ] \
-            && add_url "$2" "$links_file"
+            && add_url "$2"
         ;;
     *)
         aria2c_stop
         [ -n "$1" ] \
-            && add_url "$1" "$links_file"
+            && add_url "$1"
         aria2c_start
         ;;
 esac
