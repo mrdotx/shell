d4db44feeae492caddb9b9c029dc295dd2aa797b
mrdotx <klassiker@gmx.de>
2023-12-15 09:41:52 +0100

    option for remote server added

--- sync_notes.sh
+++ sync_notes.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/shell/sync_notes.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2023-05-15T08:28:27+0200
+# date:   2023-12-15T09:21:38+0100
 
 source_folder="$HOME/Documents/Notes/html/"
 destination_folder="/srv/http/wiki"
@@ -11,8 +11,23 @@ destination_folder="/srv/http/wiki"
 sync_to() {
     rsync -acLvh --delete \
         --exclude prinzipal.html \
-        "$source_folder" "$1":$destination_folder
+        "$source_folder" "${1:+"$1:"}$destination_folder"
 }
 
-# sync to webserver
-sync_to "m625q"
+case $@ in
+    "")
+        printf ":: sync %s to %s\n" \
+            "$source_folder" \
+            "$destination_folder"
+        sync_to
+        ;;
+    *)
+        for host_name in "$@"; do
+            printf ":: sync %s to %s on %s\n" \
+                "$source_folder" \
+                "$destination_folder" \
+                "$host_name"
+            sync_to "$host_name"
+        done
+        ;;
+esac
