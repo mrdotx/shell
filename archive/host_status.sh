a6ff9389bd1301b3142b994497039d814027223b
mrdotx <klassiker@gmx.de>
2025-02-26 06:19:48 +0100

    order of color variables changed

--- archive/host_status.sh
+++ archive/host_status.sh
@@ -3,16 +3,16 @@
 # path:   /home/klassiker/.local/share/repos/shell/archive/host_status.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2024-04-25T10:01:44+0200
+# date:   2025-02-26T06:19:42+0100
 
 # speed up script and avoid language problems by using standard c
 LC_ALL=C
 LANG=C
 
 # color variables
+reset="\033[0m"
 red="\033[31m"
 green="\033[32m"
-reset="\033[0m"
 
 # ping host and echo online or offline
 ping -c1 -W1 -q "$1" >/dev/null 2>&1 \
