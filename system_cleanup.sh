cbc7323a059a7de59fd36d6c0f23cb693fbcf5f7
mrdotx <klassiker@gmx.de>
2025-03-22 06:14:50 +0100

    output format changed

--- system_cleanup.sh
+++ system_cleanup.sh
@@ -3,7 +3,7 @@
 # path:   /home/klassiker/.local/share/repos/shell/system_cleanup.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2025-03-06T05:51:51+0100
+# date:   2025-03-22T06:14:10+0100
 
 # color variables
 reset="\033[0m"
@@ -29,7 +29,7 @@ cleanup_file() {
     ! [ -f "$1" ] \
         && return 0
 
-    printf "%b%b::%b %bCLEANUP%b file %b%s%b\n" \
+    printf "%b%b::%b %bcleanup file%b %b%s%b\n" \
         "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$1" "$reset"
     printf "remove white space from the end of the line...\n"
     sed -i 's/ *$//' "$1"
@@ -52,14 +52,14 @@ delete_files() {
     [ "$cache_files" -gt 0 ] \
         || return 0
 
-    printf "\n%b%b::%b %bDELETE%b %d files from %b%s%b not accessed in %d days...\n" \
-        "$bold" "$blue" "$reset" "$bold" "$reset" "$cache_files" "$cyan" "$1" "$reset" \
-        "$2"
+    printf "%b%b::%b %bdelete %d files from%b %b%s%b %bnot accessed in %d days...%b\n" \
+        "$bold" "$blue" "$reset" "$bold" "$cache_files" "$reset" \
+        "$cyan" "$1" "$reset" "$bold" "$2" "$reset"
 
     find_files "$1" "$2"
 
-    printf "%b%b  ->%b %bDELETE%b files from %b%s%b [y]es/[N]o: " \
-        "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$1" "$reset" \
+    printf "%b%b  ->%b delete files from %b%s%b [y]es/[N]o: " \
+        "$bold" "$blue" "$reset" "$cyan" "$1" "$reset" \
         && read -r key
     case "$key" in
         y|Y|yes|Yes)
@@ -75,32 +75,32 @@ delete_pkgs() {
     auth="${EXEC_AS_USER:-sudo}"
 
     dry_run=$($auth find "$1" -type d \
-        -exec printf "%b%b::%b %bDELETE%b pkgs from %b{}%b, except last %s versions\n" \
-            "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$reset" "$2" \; \
+        -exec printf "%b%b::%b %bdelete pkgs from%b %b{}%b%b, except last %d versions%b\n" \
+            "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$reset" \
+            "$bold" "$2" "$reset" \; \
         -exec paccache -dvk "$2" -c {} \; \
+        | sed '/^$/d' \
     )
 
-    printf "%s\n" \
-        "$dry_run" \
+    printf "%s" "$dry_run" \
         | grep -q "Candidate packages:" \
         || return 0
 
-    printf "\n%s\n" \
-        "$dry_run"
-    printf "%b%b  ->%b %bDELETE%b pkgs from %b%s%b [y]es/[N]o: " \
-        "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$1" "$reset" \
+    printf "%s\n" "$dry_run"
+    printf "%b%b  ->%b delete pkgs from %b%s%b [y]es/[N]o: " \
+        "$bold" "$blue" "$reset" "$cyan" "$1" "$reset" \
         && read -r clear_pkgs \
         && case "$clear_pkgs" in
             y|Y|yes|Yes)
                 $auth find "$1" -type d \
-                    -exec paccache -rvk "$2" -c {} \;
+                    -exec paccache -rvk "$2" -c {} \; \
+                    | sed '/^$/d'
                 ;;
         esac
 }
 
 # main
 cleanup_file "$HOME/.local/share/iwctl/history"
-printf "\n"
 cleanup_file "$HOME/.local/share/cmd_history"
 delete_files "$HOME/.cache" 365
 delete_pkgs "/srv/pacman/core/os/x86_64" 2
