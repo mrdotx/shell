8ed8634220f5c954d9585991e1fc2167383f5f4f
mrdotx <klassiker@gmx.de>
2025-03-29 06:03:01 +0100

    colors added

--- sync_notes.sh
+++ sync_notes.sh
@@ -3,10 +3,16 @@
 # path:   /home/klassiker/.local/share/repos/shell/sync_notes.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2023-12-15T09:21:38+0100
+# date:   2025-03-29T06:02:34+0100
 
 source_folder="$HOME/Documents/Notes/html/"
 destination_folder="/srv/http/wiki"
+
+# color variables
+reset="\033[0m"
+bold="\033[1m"
+blue="\033[94m"
+cyan="\033[96m"
 
 sync_to() {
     rsync -acLvh --delete \
@@ -16,17 +22,19 @@ sync_to() {
 
 case $@ in
     "")
-        printf ":: sync %s to %s\n" \
-            "$source_folder" \
-            "$destination_folder"
+        printf "%b%b::%b %bsync%b %b%s%b %bto%b %b%s%b\n" \
+            "$bold" "$blue" "$reset" "$bold" "$reset" \
+            "$cyan" "$source_folder" "$reset" "$bold" "$reset" \
+            "$cyan" "$destination_folder" "$reset"
         sync_to
         ;;
     *)
         for host_name in "$@"; do
-            printf ":: sync %s to %s on %s\n" \
-                "$source_folder" \
-                "$destination_folder" \
-                "$host_name"
+            printf "%b%b::%b %bsync%b %b%s%b %bto%b %b%s%b %bon%b %b%s%b\n" \
+            "$bold" "$blue" "$reset" "$bold" "$reset" \
+                "$cyan" "$source_folder" "$reset" "$bold" "$reset" \
+                "$cyan" "$destination_folder" "$reset" "$bold" "$reset" \
+                "$cyan" "$host_name" "$reset"
             sync_to "$host_name"
         done
         ;;
