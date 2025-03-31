b6bf8d99e93e2bd4a1375dd86f22db2bb3ce586d
mrdotx <klassiker@gmx.de>
2025-03-31 07:00:07 +0200

    colors added to output

--- archive/alpha_restore.sh
+++ archive/alpha_restore.sh
@@ -3,7 +3,14 @@
 # path:   /home/klassiker/.local/share/repos/shell/archive/alpha_restore.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2024-09-03T10:03:33+0200
+# date:   2025-03-31T06:55:29+0200
+
+# color variables
+reset="\033[0m"
+bold="\033[1m"
+green="\033[32m"
+blue="\033[94m"
+cyan="\033[96m"
 
 # help
 script=$(basename "$0")
@@ -16,7 +23,6 @@ help="$script [-h/--help] -- script to find png files with suspicious data
     $script test.png
     $script test1.png test2.png test3.png"
 
-
 if [ "$1" = "-h" ] || [ "$1" = "--help" ] || [ $# -eq 0 ]; then
     printf "%s\n" "$help"
     exit 1
@@ -24,13 +30,14 @@ else
     tmp_directory=$(mktemp -t -d alpha_restore.XXXXXX)
     suspicious=0
 
-    printf ":: suspicious files\n"
+    printf "%b%b::%b %bsuspicious files%b\n" \
+        "$bold" "$blue" "$reset" "$bold" "$reset"
     for f in "$@"; do
         magick "$f" -strip -alpha extract "$tmp_directory/alpha_extract.rgb"
         if ! (hexdump -ve '"%.2x"' "$tmp_directory/alpha_extract.rgb" \
                 | grep -q '^f*$' \
             ); then
-            printf " %s\n" "$f"
+            printf "%s\n" "$f"
             cp "$f" "$tmp_directory"
             magick -strip -alpha off "$f" \
                 "$tmp_directory/$(basename "$f").noalpha.png"
@@ -39,6 +46,8 @@ else
     done
 
     [ $suspicious -eq 1 ] \
-        && printf "\n:: please inspect files in %s\n" "$tmp_directory" \
-        || printf "\n:: no suspicious files\n"
+        && printf "%b%b==>%b please inspect files in %b%s%b\n" \
+            "$bold" "$green" "$reset" "$cyan" "$tmp_directory" "$reset"\
+        || printf "%b%b==>%b %bno suspicious files%b\n" \
+            "$bold" "$green" "$reset" "$bold" "$reset"
 fi
