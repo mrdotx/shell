5e6dd48cfdb985932a5817d7a116ead9bafe8329
mrdotx <klassiker@gmx.de>
2025-04-09 06:24:14 +0200

    delete unused packages added

--- system_cleanup.sh
+++ system_cleanup.sh
@@ -3,11 +3,20 @@
 # path:   /home/klassiker/.local/share/repos/shell/system_cleanup.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2025-03-22T06:14:10+0100
+# date:   2025-04-09T06:21:42+0200
+
+# speed up script and avoid language problems by using standard c
+LC_ALL=C
+LANG=C
+
+# auth can be something like sudo -A, doas -- or nothing,
+# depending on configuration requirements
+auth="${EXEC_AS_USER:-sudo}"
 
 # color variables
 reset="\033[0m"
 bold="\033[1m"
+green="\033[32m"
 blue="\033[94m"
 cyan="\033[96m"
 
@@ -26,8 +35,7 @@ find_files() {
 }
 
 cleanup_file() {
-    ! [ -f "$1" ] \
-        && return 0
+    ! [ -f "$1" ] && printf "missing: %b%s%b\n" "$cyan" "$1" "$reset" && return
 
     printf "%b%b::%b %bcleanup file%b %b%s%b\n" \
         "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$1" "$reset"
@@ -42,8 +50,7 @@ cleanup_file() {
 }
 
 delete_files() {
-    ! [ -d "$1" ] \
-        && return 0
+    ! [ -d "$1" ] && printf "missing: %b%s%b\n" "$cyan" "$1" "$reset" && return
 
     cache_files=$(find_files "$1" "$2" \
         | wc -l \
@@ -68,14 +75,11 @@ delete_files() {
     esac
 }
 
-delete_pkgs() {
-    ! [ -d "$1" ] \
-        && return 0
-
-    auth="${EXEC_AS_USER:-sudo}"
+delete_pkg_versions() {
+    ! [ -d "$1" ] && printf "missing: %b%s%b\n" "$cyan" "$1" "$reset" && return
 
     dry_run=$($auth find "$1" -type d \
-        -exec printf "%b%b::%b %bdelete pkgs from%b %b{}%b%b, except last %d versions%b\n" \
+        -exec printf "%b%b::%b %bdelete packages from%b %b{}%b%b, except the last %d versions%b\n" \
             "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$reset" \
             "$bold" "$2" "$reset" \; \
         -exec paccache -dvk "$2" -c {} \; \
@@ -87,7 +91,7 @@ delete_pkgs() {
         || return 0
 
     printf "%s\n" "$dry_run"
-    printf "%b%b  ->%b delete pkgs from %b%s%b [y]es/[N]o: " \
+    printf "%b%b  ->%b delete packages from %b%s%b [y]es/[N]o: " \
         "$bold" "$blue" "$reset" "$cyan" "$1" "$reset" \
         && read -r clear_pkgs \
         && case "$clear_pkgs" in
@@ -99,10 +103,80 @@ delete_pkgs() {
         esac
 }
 
-# main
+delete_unused_pkgs() {
+    ! [ -d "$1" ] && printf "missing: %b%s%b\n" "$cyan" "$1" "$reset" && return
+    ! [ -d "$2" ] && printf "missing: %b%s%b\n" "$cyan" "$2" "$reset" && return
+
+    pkgs_cache="$1"
+    pkgs_all="$2/client_pkgs"
+    pkgs_to_delete="$2/pkgs_to_delete.txt"
+    pkgs_installed="$2/pkgs_installed.txt"
+    pkgs_exceptions="$2/pkgs_exceptions.txt"
+
+    printf "%b%b::%b %bdelete unused packages from%b %b%s%b\n" \
+        "$bold" "$blue" "$reset" "$bold" "$reset" "$cyan" "$pkgs_cache" "$reset"
+
+    # process packages to delete
+    printf "%b%b==>%b create %b%s%b\n" \
+        "$bold" "$green" "$reset" "$cyan" "$pkgs_to_delete" "$reset"
+    find "$pkgs_cache" -type f -print0 \
+        | xargs -0 basename -a 2>/dev/null \
+        | sort -u > "$pkgs_to_delete"
+
+    printf "%b%b==>%b create %b%s%b\n" \
+        "$bold" "$green" "$reset" "$cyan" "$pkgs_installed" "$reset"
+    cat "$pkgs_all"/pkgs_all_*.txt \
+        | sort -u > "$pkgs_installed"
+
+    printf "%b%b==>%b delete exceptions from %b%s%b\n" \
+        "$bold" "$green" "$reset" "$cyan" "$pkgs_to_delete" "$reset"
+    sort -u -r "$pkgs_exceptions" \
+        | while IFS= read -r line; do
+            sed -i "/^$line\-/d" "$pkgs_to_delete"
+        done
+
+    printf "%b%b==>%b delete installed packages from %b%s%b\n" \
+        "$bold" "$green" "$reset" "$cyan" "$pkgs_to_delete" "$reset"
+    sort -u -r "$pkgs_installed" \
+        | while IFS= read -r line; do
+            sed -i "/^$line\-[A-Z0-9]/d" "$pkgs_to_delete"
+        done
+
+    # delete packages from cache
+    [ -s "$pkgs_to_delete" ] \
+        && cat "$pkgs_to_delete" \
+        && printf "%b%b  ->%b delete unused packages from %b%s%b [y]es/[N]o: " \
+            "$bold" "$blue" "$reset" "$cyan" "$pkgs_cache" "$reset" \
+        && read -r delete_cache
+
+    case "$delete_cache" in
+        y|Y|yes|Yes)
+            while IFS= read -r line ;do
+                $auth find "$pkgs_cache" -name "$line" -delete
+            done < "$pkgs_to_delete"
+            ;;
+    esac
+}
+
+# files
 cleanup_file "$HOME/.local/share/iwctl/history"
 cleanup_file "$HOME/.local/share/cmd_history"
 delete_files "$HOME/.cache" 365
-delete_pkgs "/srv/pacman/core/os/x86_64" 2
-delete_pkgs "/srv/pacman/extra/os/x86_64" 2
-delete_pkgs "/srv/aurutils/aurbuild" 2
+
+# packages
+case "$(uname -n)" in
+    "m625q")
+        for option in "$@"; do
+            case "$option" in
+                -u | --unused)
+                    delete_unused_pkgs "/srv/pacman" "$HOME/Public/pkgsused"
+                    ;;
+                -v | --versions)
+                    delete_pkg_versions "/srv/pacman/core/os/x86_64" 2
+                    delete_pkg_versions "/srv/pacman/extra/os/x86_64" 2
+                    delete_pkg_versions "/srv/aurutils/aurbuild" 2
+                    ;;
+            esac
+        done
+    ;;
+esac
