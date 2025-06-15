9006bcd2a71a9bcb1b394d9f80e1806eb451177b
mrdotx <klassiker@gmx.de>
2025-06-15 03:20:31 +0200

    notifications added and filter changed

--- touchpad_toggle.sh
+++ touchpad_toggle.sh
@@ -3,20 +3,49 @@
 # path:   /home/klassiker/.local/share/repos/shell/touchpad_toggle.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2023-05-28T21:30:01+0200
+# date:   2025-06-15T03:20:03+0200
 
 # speed up script and avoid language problems by using standard c
 LC_ALL=C
 LANG=C
 
+# config
+message_title="Touchpad"
+
+# helper
+notification() {
+    case "$1" in
+        -n | --notify)
+            notify-send \
+                -t 2000 \
+                -u low \
+                "$message_title $2" \
+                -h string:x-canonical-private-synchronous:"$message_title"
+            ;;
+    esac
+}
+
 device="$(xinput list \
-    | grep -P '(?<= )[\w\s:]*(?i)(touchpad|synaptics)(?-i).*?(?=\s*id)' -o \
+    | sed -nre '/[tT]ouch[pP]ad/s/.*id=([0-9]*).*/\1/p' \
     | head -n1 \
 )"
 
-if [ "$(xinput list-props "$device" \
-    | grep -P ".*Device Enabled.*\K.(?=$)" -o)" = "1" ]; then
+status="$(xinput list-props "$device" \
+    | grep "Device Enabled" \
+    | grep -o "[01]$" \
+)"
+
+# main
+case "$status" in
+    1)
         xinput disable "$device"
-else
-    xinput enable "$device"
-fi
+        notification "$1" "DISABLED"
+        ;;
+    0)
+        xinput enable "$device"
+        notification "$1" "ENABLED"
+        ;;
+    *)
+        notification "$1" "NOT FOUND"
+        ;;
+esac
