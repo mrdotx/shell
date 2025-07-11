b38c65b9817b0ee0e56dcf6cd40d8371d3ec77ae
mrdotx <klassiker@gmx.de>
2025-07-11 05:47:42 +0200

    info and bar options added

--- fritzbox.sh
+++ fritzbox.sh
@@ -3,31 +3,26 @@
 # path:   /home/klassiker/.local/share/repos/shell/fritzbox.sh
 # author: klassiker [mrdotx]
 # github: https://github.com/mrdotx/shell
-# date:   2024-08-29T05:57:45+0200
+# date:   2025-07-11T05:46:18+0200
 
-get_fritzbox_data() {
-connection_state="
-<?xml version='1.0' encoding='utf-8'?>
-    <s:Envelope xmlns:s='http://schemas.xmlsoap.org/soap/envelope/'
-     s:encodingStyle='http://schemas.xmlsoap.org/soap/encoding/'>
-        <s:Body>
-            <u:$3 xmlns:u='urn:schemas-upnp-org:service:$2:1' />
-        </s:Body>
-    </s:Envelope>
-"
+# config
+url="http://10.10.10.10:49000"
+max_down=150
+max_up=30
 
-    curl -fsS "http://fritz.box:49000/igdupnp/control/$1" \
-            -H "SoapAction:urn:schemas-upnp-org:service:$2:1#$3" \
-            -H 'User-Agent: AVM UPnP/1.0 Client 1.0' \
-            -H 'Content-Type: text/xml; charset="utf-8"' \
-            -d "$connection_state"
+# helper
+soap_igdupnp() {
+    curl -fs "$url/igdupnp/control/$1" \
+        -H 'Content-Type:text/xml' \
+        -H "SoapAction:urn:schemas-upnp-org:service:$2:1#$3" \
+        -d "<?xml?><s:Envelope xmlns:s=''><s:Body><u:$3/></s:Body></s:Envelope>"
 }
 
-get_value() {
+xml_value() {
     filter="$1"
     shift
 
-    printf "%s\n" "$@" \
+    printf "%s\n" "$*" \
         | grep "$filter" \
         | sed -e "s/<$filter>//" -e "s/<\/$filter>//"
 }
@@ -36,67 +31,98 @@ calc() {
     printf "%s\n" "$*" | bc -l
 }
 
-convert_bytes() {
-    [ "$1" -le 1000 ] \
-        && printf "0 KB/s" \
-        && exit
+conv() {
+    [ -z "$1" ] && printf "? %s" "$3" && exit 1
 
-    [ "$1" -le 1000000 ] \
-        && printf "%.0f KB/s" "$(calc "$1 / 1000")" \
-        && exit
-
-    printf "%.1f MB/s" "$(calc "$1 / 1000000")"
+    [ "$1" -le "$2" ] && printf "0 %s" "$3" && exit
+    [ "$1" -le "$4" ] && printf "%.0f %s" "$(calc "$1 / $2")" "$3" && exit
+    printf "%.1f %s" "$(calc "$1 / $4")" "$5"
 }
 
-convert_bits() {
-    [ "$1" -le 128 ] \
-        && printf "0 Kbit/s" \
-        && exit
+bit_perc() {
+    [ -z "$1" ] && printf "?.?%%" && exit 1
 
-    [ "$1" -le 131072 ] \
-        && printf "%.0f Kbit/s" "$(calc "$1 / 128")" \
-        && exit
-
-    printf "%.1f Mbit/s" "$(calc "$1 / 131072")"
+    printf "%.1f%%" "$(calc "$1 / ($2 * 131072 / 100)")"
 }
 
+bit_sec() {
+    conv "$1" 128 "Kbit/s" 131072 "Mbit/s"
+}
+
+byte_sec() {
+    conv "$1" 1000 "KB/s" 1000000 "MB/s"
+}
+
+byte_total() {
+    conv "$1" 1000000 "MB" 1000000000 "GB"
+}
+
+# main
 case "$1" in
-    --ipv4)
-        data=$( \
-            get_fritzbox_data \
-                "WANIPConn1" \
-                "WANIPConnection" \
+    -4 | --ipv4)
+        data=$(soap_igdupnp "WANIPConn1" "WANIPConnection" \
                 "GetExternalIPAddress" \
         )
 
-        get_value "NewExternalIPAddress" "$data"
+        xml_value "NewExternalIPAddress" "$data"
         ;;
-    --ipv6)
-        data=$( \
-            get_fritzbox_data \
-                "WANIPConn1" \
-                "WANIPConnection" \
+    -6 | --ipv6)
+        data=$(soap_igdupnp "WANIPConn1" "WANIPConnection" \
                 "X_AVM_DE_GetExternalIPv6Address" \
         )
 
-        get_value "NewExternalIPv6Address" "$data"
+        xml_value "NewExternalIPv6Address" "$data"
         ;;
-    *)
-        data=$( \
-            get_fritzbox_data \
-                "WANCommonIFC1" \
-                "WANCommonInterfaceConfig" \
-                "GetAddonInfos" \
+    -b | --bar)
+        data=$(soap_igdupnp "WANCommonIFC1" "WANCommonInterfaceConfig" \
+                "GetAddonInfos"
         )
 
-        byte_receive_rate=$(get_value "NewByteReceiveRate" "$data")
-        byte_send_rate=$(get_value "NewByteSendRate" "$data")
+        rate_down=$(xml_value "NewByteReceiveRate" "$data")
+        rate_up=$(xml_value "NewByteSendRate" "$data")
 
-        printf "↑ %s %s\n↓ %s %s\n" \
-                "$(convert_bytes "$byte_send_rate")" \
-                "$(convert_bits "$byte_send_rate")" \
-                "$(convert_bytes "$byte_receive_rate")" \
-                "$(convert_bits "$byte_receive_rate")" \
-            | column --table --table-right 2,4 --output-separator " "
+        printf "↓%s ↑%s" \
+            "$(bit_sec "$rate_down")" \
+            "$(bit_sec "$rate_up")"
+        ;;
+    -i | --info)
+        # color variables
+        reset="\033[0m"
+        bold="\033[1m"
+        red="\033[31m"
+        green="\033[32m"
+
+        data=$(soap_igdupnp "WANCommonIFC1" "WANCommonInterfaceConfig" \
+                "GetAddonInfos"
+        )
+
+        rate_down=$(xml_value "NewByteReceiveRate" "$data")
+        rate_up=$(xml_value "NewByteSendRate" "$data")
+        total_down=$(xml_value "NewX_AVM_DE_TotalBytesReceived64" "$data")
+        total_up=$(xml_value "NewX_AVM_DE_TotalBytesSent64" "$data")
+
+        printf "%s %s %b%b↑%b %s %s\n%s %s %b%b↓%b %s %s" \
+                "$(bit_sec "$rate_up")" \
+                "$(bit_perc "$rate_up" "$max_up")" \
+                "$bold" "$red" "$reset" \
+                "$(byte_total "$total_up")" \
+                "$(byte_sec "$rate_up")" \
+                "$(bit_sec "$rate_down")" \
+                "$(bit_perc "$rate_down" "$max_down")" \
+                "$bold" "$green" "$reset" \
+                "$(byte_total "$total_down")" \
+                "$(byte_sec "$rate_down")" \
+            | column --table --table-noheadings --output-separator " " \
+                --table-column right,width=6 \
+                --table-column right \
+                --table-column right,width=6 \
+                --table-column right \
+                --table-column right,width=3 \
+                --table-column right \
+                --table-column right,width=4 \
+                --table-column right
+        ;;
+    *)
+        watch --color --no-title --interval 1 "$0" --info
         ;;
 esac
