a5421e2b22b724a0faede57046982659aa2dde6a
mrdotx <klassiker@gmx.de>
2025-09-26 04:37:37 +0200

    structural optimizations

--- aurbuild.sh
+++ aurbuild.sh
@@ -3,70 +3,68 @@
 # path:   /home/klassiker/.local/share/repos/shell/aurbuild.sh
 # author: klassiker [mrdotx]
 # url:    https://github.com/mrdotx/shell
-# date:   2025-08-11T04:49:37+0200
+# date:   2025-09-26T04:36:47+0200
 
 # speed up script and avoid language problems by using standard c
 LC_ALL=C
 LANG=C
 
-## sync packages
-aur sync --upgrades --rmdeps --noconfirm --noview --nosync
-
-## sync devel packages
-set -e
-sync_name="sync_devel"
-sync_dir="/srv/aurutils/sync"
-
-# create tmp environment
-uid=$(id -u)
-mkdir -p "/tmp/aurutils-$uid" \
-    && chmod 0700 "/tmp/aurutils-$uid"
-tmp=$(mktemp --directory --tmpdir "aurutils-$uid/$sync_name.XXXXXX")
-trap 'rm -rf "$tmp"' EXIT
-
-# get packages and potential vcs packages from local db
-db=$(aur repo --list)
-vcs() {
-    printf "%s" "$db" \
-        | cut -d'	' -f1 \
-        | grep -E "$1" 2>/dev/null
+# functions
+create_tmp_env() {
+    mkdir -m 0700 -p "$1"
+    tmp_dir=$(mktemp --directory "$1/$sync_name.XXXXXX")
+    trap 'rm -rf "$tmp_dir"' EXIT
 }
 
-# fetch aur to compare vcs heads
-cd "$sync_dir"
-vcs "\-git$|\-cvs$|\-svn$|\-bzr$|\-darcs$|\-always$|\-hg$|\-fossil$" \
-    | aur fetch --existing --discard --sync "auto" --results "$tmp/fetch" -
+create_fetch_list() {
+    vcs_filter="\-git$|\-cvs$|\-svn$|\-bzr$|\-darcs$|\-always$|\-hg$|\-fossil$"
+    printf "%s" "$1" \
+        | cut -d'	' -f1 \
+        | grep -E "$vcs_filter" 2>/dev/null \
+        | aur fetch --existing --discard --sync=auto --results="$2" -
+}
 
-while IFS=: read -r mode rev_old rev path; do
-    vcs_name=${path##*/}
+create_build_list() {
+    # compare vcs heads
+    while IFS=: read -r mode rev_old rev path; do
+        vcs_name=${path##*/}
+        case $mode in
+            clone | merge | fetch | rebase | reset)
+                [ "$rev" != 0 ] \
+                    && [ "$rev" = "$rev_old" ] \
+                    && case "$targets" in
+                        "") targets=$(printf "%s" "$vcs_name");;
+                        *)  targets=$(printf "%s\n%s" "$targets" "$vcs_name");;
+                    esac
+                ;;
+        esac
+    done < "$1"
 
-    case $mode in
-        clone | merge | fetch | rebase | reset)
-            [ "$rev" != 0 ] \
-                && [ "$rev" = "$rev_old" ] \
-                && case "$targets" in
-                    "")
-                        targets=$(printf "%s" "$vcs_name")
-                        ;;
-                    *)
-                        targets=$(printf "%s\n%s" "$targets" "$vcs_name")
-                        ;;
-                esac
-            ;;
-    esac
-done < "$tmp/fetch"
+    # compare db version with target version to build
+    [ -n "$targets" ] \
+        && printf "%s\n" "$targets" \
+            | aur srcver - > "$tmp_dir/targets" \
+        && printf "%s\n" "$2" \
+            | aur vercmp --quiet --path="$tmp_dir/targets" > "$3"
+}
 
-# compare db version with target version to build
-[ -n "$targets" ] \
-    && printf "%s\n" "$targets" \
-        | aur srcver - > "$tmp/targets" \
-    && printf "%s\n" "$db" \
-        | aur vercmp --quiet --path "$tmp/targets" > "$tmp/builds"
+rebuild_pks() {
+    [ -s "$1" ] \
+        && aur build --syncdeps --rmdeps --noconfirm --nosync \
+            --arg-file "$1" \
+        && exit
+    printf "%s: all packages up-to-date\n" "$sync_name"
+}
 
-# rebuild the updateable packages
-[ -s "$tmp/builds" ] \
-    && aur build --syncdeps --rmdeps --noconfirm --nosync \
-        --arg-file "$tmp/builds" \
-    && exit 0
+# main
+# sync packages
+aur sync --upgrades --rmdeps --noconfirm --noview --nosync
 
-printf "%s: all packages up-to-date\n" "$sync_name"
+# sync devel packages
+cd "/srv/aurutils/sync" || exit
+sync_name="sync_devel"
+create_tmp_env "/tmp/aurutils-$(id -u)"
+local_db=$(aur repo --list)
+create_fetch_list "$local_db" "$tmp_dir/fetch"
+create_build_list "$tmp_dir/fetch" "$local_db" "$tmp_dir/builds"
+rebuild_pks "$tmp_dir/builds"
