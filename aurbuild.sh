c4a0f1fbb7416ba532471c156683b15c0f9bcf44
mrdotx <klassiker@gmx.de>
2026-02-15 06:23:09 +0100

    output optimized

--- aurbuild.sh
+++ aurbuild.sh
@@ -3,11 +3,17 @@
 # path:   /home/klassiker/.local/share/repos/shell/aurbuild.sh
 # author: klassiker [mrdotx]
 # url:    https://github.com/mrdotx/shell
-# date:   2026-02-01T05:15:13+0100
+# date:   2026-02-15T06:20:56+0100
 
 # speed up script and avoid language problems by using standard c
 LC_ALL=C
 LANG=C
+
+# color variables for interactive shell
+tty -s \
+    && reset="\033[0m" \
+    && bold="\033[1m" \
+    && green="\033[32m"
 
 # functions
 create_tmp_env() {
@@ -16,25 +22,24 @@ create_tmp_env() {
     trap 'rm -rf "$tmp_dir"' EXIT
 }
 
-create_fetch_list() {
-    vcs_filter="\-git$|\-cvs$|\-svn$|\-bzr$|\-darcs$|\-always$|\-hg$|\-fossil$"
+create_pkg_list() {
     printf "%s" "$1" \
         | cut -d'	' -f1 \
-        | grep -E "$vcs_filter" 2>/dev/null \
-        | aur fetch --existing --discard --sync=auto --results="$2" -
+        | grep -E "$2" 2>/dev/null \
+        | aur fetch --existing --discard --sync=auto --results="$3" -
 }
 
 create_build_list() {
     # compare vcs heads
     while IFS=: read -r mode rev_old rev path; do
-        vcs_name=${path##*/}
+        pkg_name=${path##*/}
         case $mode in
             clone | merge | fetch | rebase | reset)
                 [ "$rev" != 0 ] \
                     && [ "$rev" = "$rev_old" ] \
                     && case "$targets" in
-                        "") targets=$(printf "%s" "$vcs_name");;
-                        *)  targets=$(printf "%s\n%s" "$targets" "$vcs_name");;
+                        "") targets=$(printf "%s" "$pkg_name");;
+                        *)  targets=$(printf "%s\n%s" "$targets" "$pkg_name");;
                     esac
                 ;;
         esac
@@ -48,7 +53,7 @@ create_build_list() {
             | aur vercmp --quiet --path="$tmp_dir/targets" > "$3"
 }
 
-build_pks() {
+build_pkgs() {
     [ -s "$1" ] \
         && aur build --syncdeps --rmdeps --noconfirm --nosync --arg-file "$1" \
         && return
@@ -64,6 +69,9 @@ cd "/srv/aurutils/sync" || exit
 sync_name="build"
 create_tmp_env "/tmp/aurutils-$(id -u)"
 local_db=$(aur repo --list)
-create_fetch_list "$local_db" "$tmp_dir/fetch"
-create_build_list "$tmp_dir/fetch" "$local_db" "$tmp_dir/builds"
-build_pks "$tmp_dir/builds"
+pkg_filter="\-git$|\-cvs$|\-svn$|\-bzr$|\-darcs$|\-always$|\-hg$|\-fossil$"
+printf >&2 "%b%b==>%b %bUsing [%s] package filter%b\n" \
+        "$bold" "$green" "$reset" "$bold" "$pkg_filter" "$reset"
+create_pkg_list "$local_db" "$pkg_filter" "$tmp_dir/pkgs"
+create_build_list "$tmp_dir/pkgs" "$local_db" "$tmp_dir/builds"
+build_pkgs "$tmp_dir/builds"
