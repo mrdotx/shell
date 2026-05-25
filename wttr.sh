dbf1424a01afcba887739c37fe76c883685e3950
mrdotx <klassiker@gmx.de>
2026-05-25 05:00:53 +0200

    initial upload

---
+++ wttr.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# path:   /home/klassiker/.local/share/repos/shell/wttr.sh
+# author: klassiker [mrdotx]
+# url:    https://github.com/mrdotx/shell
+# date:   2026-05-25T05:00:39+0200
+
+location_cache() {
+    grep -q -s '[^[:space:]]' "$1" \
+        || curl -fsS 'https://ipinfo.io/city' > "$1"
+
+    cat "$1"
+}
+
+city=$(location_cache /tmp/location.cache | sed 's/ /%20/g')
+
+case $1 in
+    --clean)
+        curl -fsS "v2d.wttr.in/$city?AFq" | uniq | sed '1d'
+        ;;
+    --default)
+        curl -fsS "wttr.in/$city"
+        ;;
+    *)
+        curl -fsS "v2d.wttr.in/$city?AF"
+        ;;
+esac
+
