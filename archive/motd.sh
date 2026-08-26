c4b0cb20bab7cad769c0e67bdfa1d434bb8bfb00
mrdotx <klassiker@gmx.de>
2026-08-26 03:49:35 +0200

    date format changed and layout optimizations

--- archive/motd.sh
+++ archive/motd.sh
@@ -3,13 +3,14 @@
 # path:   /home/klassiker/Projects/repos/shell/archive/motd.sh
 # author: klassiker [mrdotx]
 # url:    https://github.com/mrdotx/shell
-# date:   2026-07-16T04:51:14+0200
+# date:   2026-08-26T03:49:32+0200
 
 # use standard C locale to avoid locale-specific issues and improve performance
 export LC_ALL=C LANG=C
 
 # config
 me=$(whoami)
+date_format="%d.%m.%Y %H:%M:%S"
 
 # layout functions
 color() {
@@ -18,7 +19,7 @@ color() {
 
 extend() {
     str="$1"
-    spaces=$((61-${#1}))
+    spaces=$((57-${#1}))
     while [ $spaces -gt 0 ]; do
         str="$str "
         spaces=$((spaces-1))
@@ -29,16 +30,16 @@ extend() {
 center() {
     str="$1"
     width=${2:-78}
-    spacesLeft=$(((width-${#1})/2))
-    spacesRight=$((width-spacesLeft-${#1}))
-    while [ $spacesLeft -gt 0 ]; do
+    spaces_left=$(((width-${#1})/2))
+    spaces_right=$((width-spaces_left-${#1}))
+    while [ $spaces_left -gt 0 ]; do
         str=" $str"
-        spacesLeft=$((spacesLeft-1))
+        spaces_left=$((spaces_left-1))
     done
 
-    while [ $spacesRight -gt 0 ]; do
+    while [ $spaces_right -gt 0 ]; do
         str="$str "
-        spacesRight=$((spacesRight-1))
+        spaces_right=$((spaces_right-1))
     done
 
     printf "%s" "$str"
@@ -58,48 +59,48 @@ sec2time() {
         mins=$((input/60))
 
         [ $days -ne 1 ] \
-            && daysPlural="s"
+            && days_plural="s"
 
         [ $hours -ne 1 ] \
-            && hoursPlural="s"
+            && hours_plural="s"
 
         [ $mins -ne 1 ] \
-            && minsPlural="s"
+            && mins_plural="s"
 
         printf "%s day%s, %s hour%s, %s minute%s" \
             "$days" \
-            "$daysPlural" \
+            "$days_plural" \
             "$hours" \
-            "$hoursPlural" \
+            "$hours_plural" \
             "$mins" \
-            "$minsPlural"
+            "$mins_plural"
     fi
 }
 
 uptime_long() {
     printf "%s [%s]" \
         "$(sec2time "$(cut -d "." -f 1 /proc/uptime)")" \
-        "$(date -d "@""$(grep btime /proc/stat | cut -d " " -f 2)" +"%d.%m.%Y %H:%M:%S")"
+        "$(date -d "@""$(grep btime /proc/stat | cut -d " " -f 2)" +"$date_format")"
 }
 
 # tty login
 tty_login() {
-    read -r loginFrom loginIP loginMonth loginDay loginTime loginYear << EOF
+    read -r login_from login_ip login_month login_day login_time login_year << EOF
         $(last "$me" -w -F -d | awk 'NR==2 { print $2,$3,$5,$6,$7,$8 }')
 EOF
 
-    loginDate="$(date -d "$loginMonth $loginDay $loginTime $loginYear" -Ins)"
+    login_date="$(date -d "$login_month $login_day $login_time $login_year" -Ins)"
 
-    case $loginDate in
+    case $login_date in
         -)
-            loginDate=$loginIP
-            loginIP=$loginFrom
+            login_date=$login_ip
+            login_ip=$login_from
             ;;
     esac
 
-    case $loginDate in
+    case $login_date in
         *T*)
-            login="$(date -d "$loginDate" +"%a, %d %B %Y, %T") [$loginIP]"
+            login="$(date -d "$login_date" +"$date_format") [$login_ip]"
             ;;
         *)
             # not enough logins
@@ -115,23 +116,23 @@ memory() {
     case $1 in
         swap)
             free -m \
-                | awk 'NR==3 { printf "Total: %sMB, Used: %sMB, Free: %sMB",$2,$3,$4; }'
+                | awk 'NR==3 { printf "total %sMB, used %sMB, free %sMB",$2,$3,$4; }'
             ;;
         *)
             free -m \
-                | awk 'NR==2 { printf "Total: %sMB, Used: %sMB, Free: %sMB, Cached: %sMB",$2,$3,$4,$7; }'
+                | awk 'NR==2 { printf "total %sMB, used %sMB, free %sMB, cached %sMB",$2,$3,$4,$7; }'
             ;;
     esac
 }
 # space
 space() {
     df -h "$1" \
-        | awk 'NR==2 { printf "Total: %sB, Used: %sB, Free: %sB, Utilized: %s",$2,$3,$4,$5; }'
+        | awk 'NR==2 { printf "total %sB, used %sB, free %sB, utilized %s",$2,$3,$4,$5; }'
 }
 
 # cpu
 cpu() {
-    printf "CPU: %sMHz, TEMP: %s'C" \
+    printf "cpu %sMHz, temp %s'C" \
         "$(($(cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq) / 1000))" \
         "$(cut -c "1-2" /sys/class/hwmon/hwmon0/temp1_input)"
 }
@@ -146,49 +147,49 @@ alive() {
 }
 
 # colors
-borderColor="0;34"
-headerXtogoColor="1;36"
-greetingsColor="1;32"
-statsLabelColor="1;33"
+border_color="0;34"
+header_logo_color="1;36"
+greetings_color="1;32"
+stats_label_color="1;33"
 
 # border
-borderLine="━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
-borderTopLine=$(color $borderColor "┏$borderLine┓")
-borderBottomLine=$(color $borderColor "┗$borderLine┛")
-borderBar=$(color $borderColor "┃")
-borderEmptyLine="$borderBar                                                                              $borderBar"
+border_line="━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+border_top_line=$(color $border_color "┏$border_line┓")
+border_bottom_line=$(color $border_color "┗$border_line┛")
+border_bar=$(color $border_color "┃")
+border_empty_line="$border_bar                                                                      $border_bar"
 
 # greetings
-greetings="$(color $greetingsColor "$(center "Welcome back, $me!" 55)")"
-greetingsDate="$(color $greetingsColor "$(center "$(date +"%a, %d %B %Y, %T")" 55)")"
-greetingsKernel="$(color $greetingsColor "$(center "$(uname -r -m)" 55)")"
-greetingsKernelVersion="$(color $greetingsColor "$(center "$(uname -v)" 55)")"
+greetings="$(color $greetings_color "$(center "Welcome back, $me!" 55)")"
+greetings_date="$(color $greetings_color "$(center "$(date +"$date_format")" 55)")"
+greetings_kernel="$(color $greetings_color "$(center "$(uname -r -m)" 55)")"
+greetings_kernel_version="$(color $greetings_color "$(center "$(uname -v)" 55)")"
 
 # header
-header="$borderTopLine\n"
-header="$header$borderBar$(color $headerXtogoColor "                   .___      __                                               ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "   ____________  __| _/_____/  |____  ___                                     ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "  /     \_  __ \/ __ |/  _ \   __\  \/  /                                     ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor " |  Y Y  \  | \/ /_/ (  <_> )  |  >    <                                      ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor " |__|_|  /__|  \____ |\____/|__| /__/\_ \                                     ")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "       \/           \/                 \/                                     ")$borderBar\n"
-header="$header$borderEmptyLine\n"
-header="$header$borderBar$(color $headerXtogoColor "                       $greetings")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "                       $greetingsDate")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "                       $greetingsKernel")$borderBar\n"
-header="$header$borderBar$(color $headerXtogoColor "                       $greetingsKernelVersion")$borderBar\n"
-header="$header$borderBottomLine"
+header="$border_top_line\n"
+header="$header$border_bar$(color $header_logo_color "                   .___      __                                       ")$border_bar\n"
+header="$header$border_bar$(color $header_logo_color "   ____________  __| _/_____/  |____  ___                             ")$border_bar\n"
+header="$header$border_bar$(color $header_logo_color "  /     \_  __ \/ __ |/  _ \   __\  \/  /                             ")$border_bar\n"
+header="$header$border_bar$(color $header_logo_color " |  Y Y  \  | \/ /_/ (  <_> )  |  >    <                              ")$border_bar\n"
+header="$header$border_bar$(color $header_logo_color " |__|_|  /__|  \____ |\____/|__| /__/\_ \                             ")$border_bar\n"
+header="$header$border_bar$(color $header_logo_color "       \/           \/                 \/                             ")$border_bar\n"
+header="$header$border_empty_line\n"
+header="$header$border_bar$(color $header_logo_color "               $greetings")$border_bar\n"
+header="$header$border_bar$(color $header_logo_color "               $greetings_date")$border_bar\n"
+header="$header$border_bar$(color $header_logo_color "               $greetings_kernel")$border_bar\n"
+header="$header$border_bar$(color $header_logo_color "               $greetings_kernel_version")$border_bar\n"
+header="$header$border_bottom_line"
 
 # labels
-label1="$borderBar $(color $statsLabelColor "Last Login....:") $(extend "$(tty_login)")$borderBar"
-label2="$borderBar $(color $statsLabelColor "Uptime........:") $(extend "$(uptime_long)")$borderBar"
-label3="$borderBar $(color $statsLabelColor "Memory........:") $(extend "$(memory)")$borderBar"
-label4="$borderBar $(color $statsLabelColor "Swap..........:") $(extend "$(memory swap)")$borderBar"
-label5="$borderBar $(color $statsLabelColor "Boot..........:") $(extend "$(space /boot)")$borderBar"
-label6="$borderBar $(color $statsLabelColor "Home..........:") $(extend "$(space /home)")$borderBar"
-label7="$borderBar $(color $statsLabelColor "System:.......:") $(extend "$(cpu)")$borderBar"
+label1="$border_bar $(color $stats_label_color "Last Login:") $(extend "$(tty_login)")$border_bar"
+label2="$border_bar $(color $stats_label_color "Uptime....:") $(extend "$(uptime_long)")$border_bar"
+label3="$border_bar $(color $stats_label_color "Memory....:") $(extend "$(memory)")$border_bar"
+label4="$border_bar $(color $stats_label_color "Swap......:") $(extend "$(memory swap)")$border_bar"
+label5="$border_bar $(color $stats_label_color "Boot......:") $(extend "$(space /boot)")$border_bar"
+label6="$border_bar $(color $stats_label_color "Home......:") $(extend "$(space /home)")$border_bar"
+label7="$border_bar $(color $stats_label_color "System....:") $(extend "$(cpu)")$border_bar"
 
 stats="$label1\n$label2\n$label3\n$label4\n$label5\n$label6\n$label7"
 
 # main
-printf "%b\n%b\n%b\n%b\n" "$header" "$borderTopLine" "$stats" "$borderBottomLine"
+printf "%b\n%b\n%b\n%b\n" "$header" "$border_top_line" "$stats" "$border_bottom_line"
